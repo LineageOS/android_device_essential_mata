1836980116a480d78aa9038ecd6136c97285eae3
Dante Russo <drusso@codeaurora.org>
2013-06-03 15:25:10 -0700

    Initial loc_api_v02 files
    Pulled from from jb-mr2-release-qcom
    change-id 8318e20b2979d9883eed440b2f5acf6d64567df9
    
    Change-Id: I5050c9cc08c59ad4b532f81389d7b67a9b7f7fe4

---
+++ loc_api/Android.mk
@@ -0,0 +1,18 @@
+ifneq ($(BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE),)
+
+LOCAL_PATH := $(call my-dir)
+
+# true if extended GPS_DELETE are supported in gps.h
+FEATURE_DELEXT := false
+
+#add QMI libraries for QMI targets
+QMI_BOARD_PLATFORM_LIST := msm8960
+QMI_BOARD_PLATFORM_LIST += msm8974
+QMI_BOARD_PLATFORM_LIST += msm8226
+QMI_BOARD_PLATFORM_LIST += msm8610
+
+ifeq ($(call is-board-platform-in-list,$(QMI_BOARD_PLATFORM_LIST)),true)
+include $(call all-subdir-makefiles)
+endif #is-board-platform-in-list
+
+endif#BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE
