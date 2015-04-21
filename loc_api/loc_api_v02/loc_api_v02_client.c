b6d82a5dfe77465cd4b1cc0f39881ed15d44faac
Jiafei Wen <jiafeiw@codeaurora.org>
2015-04-21 16:55:09 -0700

    Adding a missing break sentence
    This change adds a missing break sentence
    in switch-case.
    
    Change-Id: Iaa7ee5a1f08e65257ea2452f87071fcb7fa7d88e
    CRs-Fixed: 826803

--- loc_api/loc_api_v02/loc_api_v02_client.c
+++ loc_api/loc_api_v02/loc_api_v02_client.c
@@ -1368,6 +1368,7 @@ static bool validateRequest(
     case QMI_LOC_SET_GNSS_CONSTELL_REPORT_CONFIG_V02:
     {
         *pOutLen = sizeof(qmiLocSetGNSSConstRepConfigReqMsgT_v02);
+        break;
     }
 
     case QMI_LOC_START_DBT_REQ_V02:
