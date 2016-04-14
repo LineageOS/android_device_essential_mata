590f118f19f3408deb0f879045533d1f69f31a43
Kevin Tang <zhikait@codeaurora.org>
2016-04-13 21:56:28 -0700

    adding a remote API for SSTP
    The API allows stop command on SSTP operation, as well as
    registering listener on mcc and SSTP site info.
    
    Change-Id: I2016f757e5911074cff2b415cdb6213ddedac0e1
    CRs-Fixed: 961354

--- izat_api/izat_remote_api.h
+++ izat_api/izat_remote_api.h
@@ -42,12 +42,14 @@ typedef void (*locationUpdateCb)(UlpLocation *location,
 
 /* registers a client callback for listening to location updates
    locationCb - location callback function pointer implemented by client
+                Can not be NULL.
    clientData - an opaque data pointer from client. This pointer will be
                 provided back when the locationCb() callbacak is called.
                 This can be used by client to store application context
-                or statemachine etc. This parameter can be NULL.
+                or statemachine etc.
+                Can be NULL.
    return: an opaque pointer that serves as a request handle. This handle
-           os tp be fed to theunregisterLocationUpdater() call.
+           is to be fed to theunregisterLocationUpdater() call.
 */
 void* registerLocationUpdater(locationUpdateCb locationCb, void* clientData);
 
@@ -56,6 +58,45 @@ void* registerLocationUpdater(locationUpdateCb locationCb, void* clientData);
                            registerLocationUpdater()
 */
 void unregisterLocationUpdater(void* locationUpdaterHandle);
+
+typedef void (*errReportCb)(const char* errStr, void* clientData);
+typedef void (*sstpSiteUpdateCb)(const char* name, double lat, double lon,
+                                 float unc, int32_t uncConfidence,
+                                 void* clientData);
+typedef void (*sstpMccUpdateCb)(uint32_t mcc, const char* confidence,
+                                void* clientData);
+/* registers a client callback for listening to SSTP updates
+   siteCb - site info callback function pointer implemented by client
+            Can not be NULL.
+   mccCb  - MCC info callback function pointer implemented by client
+            Can not be NULL.
+   errCb  - callback to receive error report in string format, in case
+            of any error that might happen, and no siteCb or mccCb can
+            be called.
+            Can be NULL.
+   clientData - an opaque data pointer from client. This pointer will be
+                provided back when the locationCb() callbacak is called.
+                This can be used by client to store application context
+                or statemachine etc.
+                Can be NULL.
+   return: an opaque pointer that serves as a request handle. This handle
+           is to be fed to theunregisterLocationUpdater() call.
+*/
+void* registerSstpUpdater(sstpSiteUpdateCb siteCb, sstpMccUpdateCb mccCb,
+                          errReportCb errCb, void* clientData);
+
+/* unregisters the client callback
+   sstpUpdaterHandle - the opaque pointer from the return of
+                           registerLocationUpdater()
+*/
+void unregisterSstpUpdater(void* sstpUpdaterHandle);
+
+/* stop the current burst of SSTP updates (until next triggering event)
+   sstpUpdaterHandle - the opaque pointer from the return of
+                           registerLocationUpdater()
+*/
+void stopSstpUpdate(void* sstpUpdaterHandle);
+
 
 #ifdef __cplusplus
 } // extern "C"
