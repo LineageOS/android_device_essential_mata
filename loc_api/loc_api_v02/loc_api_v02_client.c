64401954a2304b8f87fb8d5d3e1f2d127bedd807
Saurabh Srivastava <ssrivast@codeaurora.org>
2016-04-15 02:55:44 +0530

    Adding validation check for locClientGetSizeByRespIndId() argument
    Adding validation check to ensure pointer argument is not NULL
    
    Change-Id: I690931f32a325b15865da6eedde57ff281a103b7
    CRs-Fixed: 975772

--- loc_api/loc_api_v02/loc_api_v02_client.c
+++ loc_api/loc_api_v02/loc_api_v02_client.c
@@ -2197,6 +2197,14 @@ locClientStatusEnumType locClientSupportMsgCheck(
 bool locClientGetSizeByRespIndId(uint32_t respIndId, size_t *pRespIndSize)
 {
   size_t idx = 0, respIndTableSize = 0;
+
+  // Validate input arguments
+  if(pRespIndSize == NULL)
+  {
+    LOC_LOGE("%s:%d]: size argument NULL !");
+    return false;
+  }
+
   respIndTableSize = (sizeof(locClientRespIndTable)/sizeof(locClientRespIndTableStructT));
   for(idx=0; idx<respIndTableSize; idx++ )
   {
