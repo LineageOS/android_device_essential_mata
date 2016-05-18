f1468f912922374aa42a007cde477ce5bc7b5b13
Saurabh Srivastava <ssrivast@codeaurora.org>
2016-05-19 01:30:26 +0530

    Adding size argument for loc_sync_process_ind()
    Adding a new paylod size argument for payload pointer being passed
    in to loc_sync_process_ind() API
    
    Change-Id: Iedf5a75639e95affc9dd312b5babfe59afeb3542
    CRs-Fixed: 983163

--- loc_api/loc_api_v02/loc_api_v02_client.h
+++ loc_api/loc_api_v02/loc_api_v02_client.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2011-2015, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2011-2016, The Linux Foundation. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -1505,6 +1505,7 @@ typedef void  (*locClientRespIndCbType)(
       locClientHandleType handle,
       uint32_t respIndId,
       const locClientRespIndUnionType respIndPayload,
+      uint32_t respIndPayloadSize,
       void *pClientCookie
 );
 
