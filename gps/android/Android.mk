4e4ba683ae89ac90c2d4c2df5a3240360bf55ac5
Thierry Strudel <tstrudel@google.com>
2017-04-25 14:27:11 -0700

    msm8998: fix android.hardware.gnss@1.0-service-qti
    Make service part of 'hal' class.
    Install files in vendor partition.
    Register instance as 'default' as expected by framework.
    
    Change-Id: I487fad4df872397ab76cea377e5304c956a756da
    Signed-off-by: Thierry Strudel <tstrudel@google.com>

--- gps/android/Android.mk
+++ gps/android/Android.mk
@@ -5,6 +5,7 @@ LOCAL_MODULE := android.hardware.gnss@1.0-impl-qti
 LOCAL_VENDOR_MODULE := true
 LOCAL_MODULE_OWNER := qti
 LOCAL_MODULE_RELATIVE_PATH := hw
+LOCAL_PROPRIETARY_MODULE := true
 LOCAL_SRC_FILES := \
     AGnss.cpp \
     Gnss.cpp \
@@ -60,6 +61,7 @@ ifeq ($(BUILD_GNSS_HIDL_SERVICE), true)
 include $(CLEAR_VARS)
 LOCAL_MODULE := android.hardware.gnss@1.0-service-qti
 LOCAL_MODULE_RELATIVE_PATH := hw
+LOCAL_PROPRIETARY_MODULE := true
 LOCAL_INIT_RC := android.hardware.gnss@1.0-service-qti.rc
 LOCAL_VENDOR_MODULE := true
 LOCAL_SRC_FILES := \
