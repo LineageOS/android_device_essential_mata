bbf901bee56397433480c2a52a9020939e4eecbb
Chia-I Wu <olv@google.com>
2017-09-28 11:55:45 -0700

    mata: switch to IMapper 2.1
    Change-Id: I33be48cd40457ceabc24ff5e085fa8ddaa4670e4

--- device.mk
+++ device.mk
@@ -133,7 +133,7 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.composer@2.1-impl:64 \
     android.hardware.graphics.composer@2.1-service \
-    android.hardware.graphics.mapper@2.0-impl \
+    android.hardware.graphics.mapper@2.0-impl-2.1 \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
     copybit.msm8998 \
