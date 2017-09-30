2c3f5c7897f2ff42a735dd42a2be4cd043017de3
Rashed Abdel-Tawab <rashed@linux.com>
2017-09-30 13:08:15 -0400

    mata: Build NFC service
    Change-Id: I72bfb700e3d9b2e2c9b3c482413642624f962b1c

--- device.mk
+++ device.mk
@@ -213,6 +213,8 @@ PRODUCT_COPY_FILES += \
 
 # NFC
 PRODUCT_PACKAGES += \
+    android.hardware.nfc@1.0-impl \
+    android.hardware.nfc@1.0-service \
     com.android.nfc_extras \
     nfc_nci.msm8998 \
     NfcNci \
