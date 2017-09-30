56c8c729a115e5aa4f885a3fe7c7481eecfb4c60
Rashed Abdel-Tawab <rashed@linux.com>
2017-09-30 12:53:34 -0400

    mata: Built QTI GPS impl
    Change-Id: I5f3ce4753d8ac1ae0f8fe9a254d6f01443897d05

--- device.mk
+++ device.mk
@@ -147,7 +147,9 @@ PRODUCT_PACKAGES += \
 
 # GPS
 PRODUCT_PACKAGES += \
-    gps.msm8998 \
+    android.hardware.gnss@1.0-impl-qti \
+    android.hardware.gnss@1.0-service-qti \
+    libgnss \
     libgnsspps \
     libvehiclenetwork-native
 
