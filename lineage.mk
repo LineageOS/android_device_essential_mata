ffd479295c7c42ebf6cfd2f5fb72f263de7b1ef8
Dan Pasanen <dan.pasanen@gmail.com>
2017-10-29 17:12:42 -0500

    mata: inherit common_full_phone instead of just common
    Change-Id: Ia074f92f1b9c6ea1d1c414bfdf585f548b1cb206

--- lineage.mk
+++ lineage.mk
@@ -1,5 +1,5 @@
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/cm/config/common.mk)
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
