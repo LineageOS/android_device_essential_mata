55c71ca12db8190673a924eb4cbf47a956246e39
Dan Pasanen <dan.pasanen@gmail.com>
2017-10-31 15:19:19 -0500

    mata: add wifi_concurrency_cfg

--- device.mk
+++ device.mk
@@ -249,6 +249,7 @@ PRODUCT_COPY_FILES += \
     device/essential/mata/wifi/hostapd_default.conf:system/etc/hostapd/hostapd_default.conf \
     device/essential/mata/wifi/hostapd.accept:system/etc/hostapd/hostapd.accept \
     device/essential/mata/wifi/hostapd.deny:system/etc/hostapd/hostapd.deny \
+    device/essential/mata/wifi/wifi_concurrency_cfg.txt:system/etc/wifi/wifi_concurrency_cfg.txt \
     device/essential/mata/wifi/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf \
     device/essential/mata/wifi/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf \
     device/essential/mata/wifi/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini
