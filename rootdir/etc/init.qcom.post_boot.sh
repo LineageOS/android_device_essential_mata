8dfda88fe0f404e128ea3325ba7dbe6c46da1db0
Dan Pasanen <dan.pasanen@gmail.com>
2017-11-05 19:40:33 -0600

    mata: switch to sched governor if it exists
    Change-Id: Ia3c93b8de4e45c01f3be13c51db9693329c6ade9

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -89,6 +89,10 @@ echo 79000 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/max_freq_hysteresi
 echo 300000 > /sys/devices/system/cpu/cpu4/cpufreq/scaling_min_freq
 echo 1 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/ignore_hispeed_on_notif
 
+# if EAS is present, switch to sched governor (no effect if not EAS)
+echo "sched" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+echo "sched" > /sys/devices/system/cpu/cpu4/cpufreq/scaling_governor
+
 # re-enable thermal and BCL hotplug
 echo 1 > /sys/module/msm_thermal/core_control/enabled
 
