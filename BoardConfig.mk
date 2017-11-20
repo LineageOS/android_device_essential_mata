f61af5c4808f507ec5260ef94f068bc8ad230b8c
Dan Pasanen <dan.pasanen@gmail.com>
2017-11-20 10:58:18 -0600

    mata: remove unused console flags from kernel cmdline
    Change-Id: I643c98cd2a3251926fbec1fc9c683cc6295e503c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -109,7 +109,7 @@ BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := $(TARGET_BOARD_PLATFORM)
 BOARD_USES_CYANOGEN_HARDWARE := true
 
 # Kernel
-BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=mata user_debug=31 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 cma=32M@0-0xffffffff
+BOARD_KERNEL_CMDLINE := androidboot.hardware=mata user_debug=31 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 cma=32M@0-0xffffffff
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_TAGS_OFFSET := 0x02000000
