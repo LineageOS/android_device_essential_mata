ecb8ae06966c0cd8671aa3b75d05c46934de0a85
Rashed Abdel-Tawab <rashed@linux.com>
2017-12-28 00:26:10 +0200

    mata: Add Essential privapp sysconfig

--- device.mk
+++ device.mk
@@ -248,6 +248,7 @@ PRODUCT_COPY_FILES += \
 
 # Permissions
 PRODUCT_COPY_FILES += \
+    device/essential/mata/configs/privapp-permissions-mata.xml:system/etc/permissions/privapp-permissions-mata.xml \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
