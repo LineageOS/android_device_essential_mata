0990d74450ddcbf61e67337251ddd9be1eea4977
Rashed Abdel-Tawab <rashed@linux.com>
2017-12-27 23:41:02 +0200

    mata: Add vendor to verity and use full bootdevice path

--- device.mk
+++ device.mk
@@ -343,7 +343,8 @@ PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
 
 # Verity
-PRODUCT_SYSTEM_VERITY_PARTITION := /dev/block/bootdevice/by-name/system
+PRODUCT_SYSTEM_VERITY_PARTITION := /dev/block/platform/soc/1da4000.ufshc/by-name/system
+PRODUCT_VENDOR_VERITY_PARTITION := /dev/block/platform/soc/1da4000.ufshc/by-name/vendor
 $(call inherit-product, build/target/product/verity.mk)
 
 # Vibrator
