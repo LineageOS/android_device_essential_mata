822c3c7306f6e6c536ec77ee7b95152c8219c776
Rashed Abdel-Tawab <rashed@linux.com>
2018-01-02 16:05:51 -0500

    mata: Set IMS to use CodeAurora extension
    Change-Id: I64cba319986748e4dad206ca664ccda7eefb348a

--- device.mk
+++ device.mk
@@ -179,6 +179,10 @@ PRODUCT_PACKAGES += \
     android.hidl.manager@1.0 \
     android.hidl.manager@1.0-java
 
+# IMS
+PRODUCT_PACKAGES += \
+    ims-ext-common
+
 # Init
 PRODUCT_PACKAGES += \
     fstab.mata \
