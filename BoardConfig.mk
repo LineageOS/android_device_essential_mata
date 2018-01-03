e0900b5d927ab1b1ba862def70b1dfea8f6e5b16
Rashed Abdel-Tawab <rashed@linux.com>
2018-01-02 22:51:17 -0500

    mata: Update sepolicy for oreo
    Change-Id: I0bf4d63cc43b89f3bd96ca776c50770c755e1d74

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -177,7 +177,8 @@ BOARD_ROOT_EXTRA_FOLDERS := bt_firmware dsp firmware persist
 
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
-BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
+BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 
 # Timeservice
 BOARD_USES_QC_TIME_SERVICES := true
