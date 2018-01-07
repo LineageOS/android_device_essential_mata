fd3aeae9eb95ff2216c43ae665a134de5b1d84b4
Rashed Abdel-Tawab <rashed@linux.com>
2018-01-07 13:53:04 -0800

    mata: Add QTI low power whitelist
    Change-Id: Idb4a954979f916a7903ead982bf18b33e5cf1baf

--- device.mk
+++ device.mk
@@ -296,6 +296,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
 
+# QCOM
+PRODUCT_COPY_FILES += \
+    device/essential/mata/configs/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml
+
 # QMI
 PRODUCT_PACKAGES += \
     libjson
