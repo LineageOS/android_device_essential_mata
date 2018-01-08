e42562e9c5baf30d17245f143fe047f9f8177df5
Ethan Chen <intervigil@gmail.com>
2018-01-08 00:49:26 -0800

    mata: Merge init.target.rc into init.mata.rc
    Change-Id: I97ffc4b9b4b9ef22be486af812655ab73a5614ba

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -88,15 +88,6 @@ LOCAL_MODULE_RELATIVE_PATH := init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.target.rc
-LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.target.rc
-LOCAL_VENDOR_MODULE    := true
-LOCAL_MODULE_RELATIVE_PATH := init/hw
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE       := ueventd.mata.rc
 LOCAL_MODULE_STEM  := ueventd.rc
 LOCAL_MODULE_TAGS  := optional eng
