334fde8f6b45b974c8f8f34fe76dd3429044ac21
Mikhail Naganov <mnaganov@google.com>
2018-01-08 17:05:28 -0800

    Switch to Soundtrigger 2.1 implementation
    Bug: 68823037
    Change-Id: If634345594ea6d574f49d58e29b48165c3b9458b
    Test: "ok google" and music iq from locked screen

--- device.mk
+++ device.mk
@@ -36,7 +36,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@2.0-impl \
-    android.hardware.soundtrigger@2.0-impl \
+    android.hardware.soundtrigger@2.1-impl \
     audio.a2dp.default \
     audio.primary.msm8998 \
     audio.r_submix.default \
