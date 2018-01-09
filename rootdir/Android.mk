3a3dc344241139e586676cb049ab4fa2ba74331c
Ethan Chen <intervigil@gmail.com>
2018-01-08 21:43:16 -0800

    mata: Squash msm.usb.configfs into init.qcom.usb.rc
    Change-Id: I243c8697a51a74a103ac96494c4da2eec1550401

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -39,15 +39,6 @@ LOCAL_MODULE_RELATIVE_PATH := init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.msm.usb.configfs.rc
-LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.msm.usb.configfs.rc
-LOCAL_VENDOR_MODULE    := true
-LOCAL_MODULE_RELATIVE_PATH := init/hw
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qcom.devstart.sh
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := EXECUTABLES
