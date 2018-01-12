12b05853b63e1d486c2bba0ac0c5eb0166a3304a
Rashed Abdel-Tawab <rashed@linux.com>
2018-01-12 13:43:54 -0800

    mata: Build lineage vibrator HAL
    Change-Id: Id99c615b2c15f36324afe17e41b5fee4f6667280

--- device.mk
+++ device.mk
@@ -407,8 +407,7 @@ $(call inherit-product, build/target/product/verity.mk)
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-impl \
-    android.hardware.vibrator@1.0-service
+    android.hardware.vibrator@1.0-service.lineage
 
 # VNDK
 PRODUCT_PACKAGES += vndk-sp
