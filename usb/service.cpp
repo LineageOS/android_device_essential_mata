0846472301ce1b6792b3dcc46a0e4f36ae2acf92
Badhri Jagan Sridharan <Badhri@google.com>
2018-01-19 06:08:52 -0800

    Usb Gadget hal implementation for mata
    (cherry-pick of commit: e9af4669659c306a9c9b1a1ae3a1313a7631357d)
    
    Bug: 63669128
    Test: Tested USB gadget configurations and verified
    that they enumerated.
    Change-Id: If0f98697488f6c7cfe335d4c292acebaaba6c20f

--- usb/service.cpp
+++ usb/service.cpp
@@ -17,6 +17,7 @@
 
 #include <hidl/HidlTransportSupport.h>
 #include "Usb.h"
+#include "UsbGadget.h"
 
 using android::sp;
 
@@ -26,23 +27,36 @@ using android::hardware::joinRpcThreadpool;
 
 // Generated HIDL files
 using android::hardware::usb::V1_1::IUsb;
+using android::hardware::usb::gadget::V1_0::IUsbGadget;
 using android::hardware::usb::V1_1::implementation::Usb;
+using android::hardware::usb::gadget::V1_0::implementation::UsbGadget;
 
-using android::status_t;
 using android::OK;
+using android::status_t;
 
 int main() {
 
     android::sp<IUsb> service = new Usb();
+    android::sp<IUsbGadget> service2 = new UsbGadget();
 
-    configureRpcThreadpool(1, true /*callerWillJoin*/);
+    configureRpcThreadpool(2, true /*callerWillJoin*/);
     status_t status = service->registerAsService();
 
-    if (status == OK) {
-        ALOGI("USB HAL Ready.");
-        joinRpcThreadpool();
+    if (status != OK) {
+        ALOGE("Cannot register USB HAL service");
+        return 1;
     }
 
-    ALOGE("Cannot register USB HAL service");
+    status = service2->registerAsService();
+
+    if (status != OK) {
+        ALOGE("Cannot register USB Gadget HAL service");
+        return 1;
+    }
+
+    ALOGI("USB HAL Ready.");
+    joinRpcThreadpool();
+    // Under noraml cases, execution will not reach this line.
+    ALOGI("USB HAL failed to join thread pool.");
     return 1;
 }
