4ed9b8404ed51eb00296cd9ee7df849d7e607320
Ethan Chen <intervigil@gmail.com>
2018-01-30 23:31:43 -0800

    mata: Fix nits
    * Alphabetization
    * Spacing
    
    Change-Id: I8e564dacf5b2d7ba13c0c4bfd401f525d39644ff

--- device.mk
+++ device.mk
@@ -148,11 +148,11 @@ PRODUCT_PACKAGES += \
     copybit.msm8998 \
     gralloc.msm8998 \
     hwcomposer.msm8998 \
-    memtrack.msm8998 \
     libdisplayconfig \
     liboverlay \
     libqdMetaData.system \
-    libtinyxml
+    libtinyxml \
+    memtrack.msm8998
 
 # DRM
 PRODUCT_PACKAGES += \
