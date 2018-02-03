09026f83bd6ca7049b906471f803d99dc4cdf559
Rashed Abdel-Tawab <rashed@linux.com>
2018-02-03 12:31:19 -0800

    mata: Set WiFi driver path and on/off commands
    * Allows use of WLAN driver as a built-in to the kernel
    
    Change-Id: I8c6edfb0af9962b0cb743a17379866baf3b3a6ad

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -12,15 +12,6 @@ LOCAL_VENDOR_MODULE    := true
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.mata.modprobe.rc
-LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.mata.modprobe.rc
-LOCAL_VENDOR_MODULE    := true
-LOCAL_MODULE_RELATIVE_PATH := init/hw
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE       := init.mata.ramdump.rc
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
