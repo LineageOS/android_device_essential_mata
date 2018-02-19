e0c94c4946158e96eee36556aae841bc87cb0aef
Dan Pasanen <dan.pasanen@gmail.com>
2018-02-18 22:44:59 -0600

    mata: dexpreopt everything
    * With seamless updates, it can take a very long time to
      dex2oat all user and system apps. Save some time during
      an update by dex preopting everything on system.
    
    Change-Id: I37afbadf5cfabf72c9e0558df5fa32341bec1a27

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -103,6 +103,16 @@ USE_CAMERA_STUB := true
 # Charger
 BOARD_CHARGER_ENABLE_SUSPEND := true
 
+# Dexpreopt
+ifeq ($(HOST_OS),linux)
+  ifneq ($(TARGET_BUILD_VARIANT),eng)
+    ifeq ($(WITH_DEXPREOPT),)
+      WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := false
+      WITH_DEXPREOPT := true
+    endif
+  endif
+endif
+
 # Display
 MAX_EGL_CACHE_KEY_SIZE := 12*1024
 MAX_EGL_CACHE_SIZE := 2048*1024
