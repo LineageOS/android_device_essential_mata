421dfe4a9141a134f53474850ab5e5c627599eab
Ruchi Kandoi <kandoiruchi@google.com>
2018-03-01 13:59:26 -0800

    Remove system and vendor image dependency
    libnfc-nci.conf shouldn't be a part of vendor image, since it is
    accessed by libnfc-nci and Nfc application, both are part of system
    image.
    
    Test: Nfc enable/disable
    Bug: 72080121
    Change-Id: I686ba234c7d2aa923070c3fa980c56b73b18e574
    (cherry picked from commit 3197b56882ed9997043d87e6582f6ebaa0fdbace)

--- device.mk
+++ device.mk
@@ -253,7 +253,7 @@ PRODUCT_PACKAGES += \
     Tag
 
 PRODUCT_COPY_FILES += \
-    device/essential/mata/configs/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf \
+    device/essential/mata/configs/libnfc-nci.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/libnfc-nci.conf \
     device/essential/mata/configs/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
 
 # OMX
