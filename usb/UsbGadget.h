fdfe374ad6eae0c6c6043ffe195fd161ad148651
Badhri Jagan Sridharan <badhri@google.com>
2018-03-06 12:46:40 -0800

    Set VID/PID for diag functions
    persist.vendor.usb.config stores the vendor specific functions that are
    enabled in the target when ro.bootmode is set to usbradio through
    the adb command. Since its a persistent property, the oem functions
    are persisted once enabled. Alternatively, store the oem functions
    in vendor.usb.config. vendor.usb.config is not persisted though.
    vendor.usb.config will also reflect the enabled oem functions.
    
    Configurations supported in this CL:
    1. diag
    2. diag,serial_cdev,rmnet_gsi
    
    This would work irrespective of when ADB is not or not.
    
    Bug: 74603740
    Test: Made sure the QXDM recognized wahoo when adb is enabled/
    disabled for diag and diag,serial_cdev,rmnet_gsi
    Change-Id: Icbbe177e3f8958efc36f67692b9ec935b718fb03

--- usb/UsbGadget.h
+++ usb/UsbGadget.h
@@ -23,6 +23,7 @@
 #include <android/hardware/usb/gadget/1.0/IUsbGadget.h>
 #include <hidl/MQDescriptor.h>
 #include <hidl/Status.h>
+#include <string>
 #include <sys/epoll.h>
 #include <sys/eventfd.h>
 #include <thread>
@@ -40,6 +41,7 @@ namespace implementation {
 
 using ::android::sp;
 using ::android::base::GetProperty;
+using ::android::base::SetProperty;
 using ::android::base::unique_fd;
 using ::android::base::WriteStringToFile;
 using ::android::hardware::hidl_array;
