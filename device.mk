b567e8d6969a8195ffa7d060564c500f90e896fa
Rashed Abdel-Tawab <rashed@linux.com>
2018-03-19 11:53:09 -0700

    mata: Build hexagon neuralnetworks HAL and add controller
    Change-Id: I6868d82f71f1be8dc777274b625ee62f69f816e8

--- device.mk
+++ device.mk
@@ -252,6 +252,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml
 
+# Neural Networks
+PRODUCT_PACKAGES += \
+    android.hardware.neuralnetworks@1.0-service-hvx
+
 # Network
 PRODUCT_PACKAGES += \
     netutils-wrapper-1.0
