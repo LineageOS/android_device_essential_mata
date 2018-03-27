ccdbbf76602e0b3f11f81c7b4ab4cd0a8422fc21
Jorim Jaggi <jjaggi@google.com>
2018-03-27 15:57:47 +0200

    mata: Disable sdm rotator downscaler
    Rotator down scaler is causing additional delays that are better
    handled by GL composition.
    
    Test: Open app, no jank
    Bug: 73640835
    Change-Id: I6ea9baa28acc222afcf365f3bf107149bc8974d9

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -103,6 +103,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.opengles.version=196610 \
     ro.vendor.display.cabl=2 \
     vendor.display.disable_partial_split=1 \
+    vendor.display.disable_rotator_downscale=1 \
     vendor.gralloc.enable_fb_ubwc=1
 
 # Enforce privapp-permissions whitelist
