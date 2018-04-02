f03569d13e3e6e85f3c3ef4b61364f3cba317827
Ethan Chen <intervigil@gmail.com>
2018-04-01 17:28:32 -0700

    mata: Add device settings for touchscreen
    Change-Id: Ib6bceb903e446f4947ce8e087c03d9165b138bb6

---
+++ devicesettings/src/org/lineageos/settings/device/Constants.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (C) 2018 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lineageos.settings.device;
+
+public class Constants {
+    public static final String GRIP_REJ_KEY = "touchscreen_grip_rej";
+    public static final String GRIP_REJ_PROPERTY = "sys.grip.status";
+    public static final String SMOOTHNESS_KEY = "touchscreen_smoothness";
+    public static final String SMOOTHNESS_SUMMARY = "Smoothness level: ";
+    public static final String SMOOTHNESS_PROPERTY = "sys.touch.smoothness";
+}
