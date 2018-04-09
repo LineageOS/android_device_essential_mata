6b5c6bf4dfb5f2b042f34df9ee89af54be307003
Rashed Abdel-Tawab <rashed@linux.com>
2018-04-08 17:03:34 -0700

    mata: Build a system version of android.hidl.base@1.0 for imsvt
    Change-Id: Icb911f414595117a28fb764e145fbbf6a7ee0787

--- device.mk
+++ device.mk
@@ -196,6 +196,7 @@ PRODUCT_PACKAGES += \
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
+    android.hidl.base@1.0_system \
     android.hidl.manager@1.0 \
     android.hidl.manager@1.0-java
 
