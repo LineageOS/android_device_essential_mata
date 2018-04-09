c64d343427db1b34deb10396f87c1931da86035b
Rashed Abdel-Tawab <rashed@linux.com>
2018-04-08 17:03:34 -0700

    mata: Build a system version of android.hidl.base@1.0 for imsvt
    Change-Id: Icb911f414595117a28fb764e145fbbf6a7ee0787

--- libhidl/Android.mk
+++ libhidl/Android.mk
@@ -23,6 +23,14 @@ include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_SHARED_LIBRARIES := libhidltransport
+LOCAL_MODULE := android.hidl.base@1.0_system
+LOCAL_INSTALLED_MODULE_STEM := android.hidl.base@1.0.so
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_SHARED_LIBRARIES := libhidltransport
 LOCAL_MODULE := android.hidl.manager@1.0
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
