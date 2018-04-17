20dff798ba0f4a0e5cc8f56d96130791e99cbb29
Ethan Chen <intervigil@gmail.com>
2018-04-16 22:36:28 -0700

    mata: Re-add irqbalance
    * OPM1.180104.141 has this active again
    
    Change-Id: Id82cf64e0e24f0c87a57fcc37f1e36286fc28f8f

--- device.mk
+++ device.mk
@@ -222,6 +222,10 @@ PRODUCT_PACKAGES += \
     ebtables \
     ethertypes
 
+# IRQ Balancing
+PRODUCT_COPY_FILES += \
+    device/essential/mata/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
+
 # IRSC
 PRODUCT_COPY_FILES += \
     device/essential/mata/configs/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config
