fae5b158e5c0dc3e92cf9d305f8d1b8b14763c01
Yifan Hong <elsk@google.com>
2018-04-25 21:17:28 +0000

    Revert "health HAL: overrides healthd."
    This reverts commit 6c41af4807fb8bb103ce0872f73384ceb5cb99eb.
    This is a partial revert; CleanSpec.mk is not reverted.
    
    Reason for revert: prevents boot with O-MR1 vendor + P system
    
    Test: boots
    
    Change-Id: Ib573f525da836f12201fd0f9793441a9fed31969
    Fixes: 78550051

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -133,7 +133,6 @@ BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := $(TARGET_BOARD_PLATFORM)
 
 # HIDL
 DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/framework_manifest.xml
-DEVICE_FRAMEWORK_MANIFEST_FILE += system/libhidl/vintfdata/manifest_healthd_exclude.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
 TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
