ec281c8f7a09db29d3742b76d7af5c3bf69ab754
Chris Fries <cfries@google.com>
2018-05-04 21:38:44 -0500

    mata: set TARGET_ENABLE_MEDIADRM_64
    Use 64 bit crypto libraries for DRM
    
    Bug: 77822369
    Change-Id: Ic3309d0c3bd82a37b52c5cb765b007ab2ae2555b

--- device.mk
+++ device.mk
@@ -148,7 +148,7 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-impl:32 \
+    android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
     android.hardware.drm@1.1-service.clearkey \
     move_widevine_data.sh
