725a268d21160fcc70f4dd5d45121947535b600e
Rashed Abdel-Tawab <rashed@linux.com>
2018-05-08 12:42:57 -0700

    mata: Set ro.vndk.version to 27
    Change-Id: Ia4b72178f7dfb6a359078de7f5af6d07f205bdb7

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -126,6 +126,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.usb.config.extra=none
 
 # VNDK
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    ro.vndk.version=27
+
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.vndk.version=27.1.0
 
