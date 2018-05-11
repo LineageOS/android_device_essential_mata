c9b6a24c495495c809c9168f470e3ed0a03d95e2
Bruno Martins <bgcngm@gmail.com>
2018-05-11 23:32:31 +0100

    mata: Restore audio tuning mixer control
     * After all, this still seems to be needed for ACDB MCS init,
       as seen bellow. Just let ACDB loader be happy.
    
       Without the tuning mixer file:
         E ACDB-MCS: acdb_mcs_init: /vendor/etc/audio_tuning_mixer_tasha.txt not present, using /vendor/etc/audio_tuning_mixer.txt config file
         E MCS-RT-CTL: Can't open the configuration file /vendor/etc/audio_tuning_mixer.txt.
         E ACDB-MCS: acdb_mcs_init: MCS routing control initialization failed.
    
       With the tuning mixer file:
         E ACDB-MCS: acdb_mcs_init: /vendor/etc/audio_tuning_mixer_tasha.txt not present, using /vendor/etc/audio_tuning_mixer.txt config file
         D         : [ACPH]->MCS RTC service registered with ACPH
    
    Change-Id: I6e4b1fe27547a354eaa4132aba8e3df04d6184b3

--- device.mk
+++ device.mk
@@ -65,6 +65,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
 
+PRODUCT_COPY_FILES += \
+    hardware/qcom-caf/msm8998/audio/configs/msm8998/audio_tuning_mixer.txt:$(TARGET_COPY_OUT_VENDOR)/etc/audio_tuning_mixer.txt \
+    hardware/qcom-caf/msm8998/audio/configs/msm8998/audio_tuning_mixer_tavil.txt:$(TARGET_COPY_OUT_VENDOR)/etc/audio_tuning_mixer_tavil.txt
+
 # A/B
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
