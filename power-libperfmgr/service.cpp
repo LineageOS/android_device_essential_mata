5a31e3fcfab60389c93a15f84a120b2bca142594
Michael Wright <michaelwr@google.com>
2018-06-21 02:38:28 +0100

    mata: Update power HAL to version 1.3.
    This adds support for the EXPENSIVE_RENDERING power hint.
    
    BUG: 110112323
    Test: adb shell cat /sys/class/kgsl/kgsl-3d0/devfreq/cur_freq
          Verified GPU frequency reset by adb shell pkill surfaceflinger and system_server
    Change-Id: Ia1f6e99b67c5e88b610412ad30f152d241095a48
    Merged-In: Ia1f6e99b67c5e88b610412ad30f152d241095a48

--- power-libperfmgr/service.cpp
+++ power-libperfmgr/service.cpp
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-#define LOG_TAG "android.hardware.power@1.2-service.mata-libperfmgr"
+#define LOG_TAG "android.hardware.power@1.3-service.mata-libperfmgr"
 
 #include <android/log.h>
 #include <hidl/HidlTransportSupport.h>
@@ -30,11 +30,11 @@ using android::hardware::configureRpcThreadpool;
 using android::hardware::joinRpcThreadpool;
 
 // Generated HIDL files
-using android::hardware::power::V1_2::IPower;
-using android::hardware::power::V1_2::implementation::Power;
+using android::hardware::power::V1_3::IPower;
+using android::hardware::power::V1_3::implementation::Power;
 
 int main(int /* argc */, char** /* argv */) {
-    ALOGI("Power HAL Service 1.2 for Mata is starting");
+    ALOGI("Power HAL Service 1.3 for Mata is starting");
 
     android::sp<IPower> service = new Power();
     if (service == nullptr) {
