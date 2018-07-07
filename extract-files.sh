4452695b347eb16b63d181db44169ce0a9dfda53
Rashed Abdel-Tawab <rashed@linux.com>
2018-07-07 09:24:56 -0700

    mata: Add recovery touch support
    Change-Id: I9772f5c60b9cbe05d02a36ef85e9ea6306cfac40

--- extract-files.sh
+++ extract-files.sh
@@ -56,6 +56,7 @@ fi
 setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT" false "$CLEAN_VENDOR"
 
 extract "$MY_DIR"/proprietary-files.txt "$SRC" "$SECTION"
+extract "$MY_DIR"/proprietary-files-recovery.txt "$SRC" "$SECTION"
 
 sed -i 's/service fps_hal_mata/service vendor.fps_hal_mata/g' "$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary/vendor/etc/init/android.hardware.biometrics.fingerprint@2.1-service.mata.rc
 sed -i 's/service sidecar-hal-1-0/service vendor.sidecar-hal-1-0/g' "$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary/vendor/etc/init/vendor.essential.hardware.sidecar@1.0-service.rc
