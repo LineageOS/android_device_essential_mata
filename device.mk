153e95be12f42463a5c7491bbb5752d845362389
Rashed Abdel-Tawab <rashed@linux.com>
2018-07-25 11:49:27 -0700

    mata: usb: Update to usb@1.1
    Change-Id: Iff7aaeaca6f54f53442e89952485afbed2c297c1

--- device.mk
+++ device.mk
@@ -389,7 +389,7 @@ PRODUCT_PACKAGES_DEBUG += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service.mata
+    android.hardware.usb@1.1-service.mata
 
 # Verity
 PRODUCT_SYSTEM_VERITY_PARTITION := /dev/block/platform/soc/1da4000.ufshc/by-name/system
