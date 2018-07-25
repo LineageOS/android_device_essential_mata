f0a1a16b325be2a50c7f9b55bcc7d9160cd4f310
Rashed Abdel-Tawab <rashed@linux.com>
2018-07-25 11:21:07 -0700

    mata: Import USB HAL from hardware/interfaces
    Copied from 01787f47f4ee024889c13ec3a2b30624f6d5745d
    
    Change-Id: I85c41fa0844adea68336a368de3bec1f5a60d3d9

--- device.mk
+++ device.mk
@@ -406,7 +406,7 @@ PRODUCT_PACKAGES_DEBUG += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service
+    android.hardware.usb@1.0-service.mata
 
 # Verity
 PRODUCT_SYSTEM_VERITY_PARTITION := /dev/block/platform/soc/1da4000.ufshc/by-name/system
