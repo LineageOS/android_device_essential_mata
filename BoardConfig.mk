c0a37b438e9727aa8485358f3f5d9721bd8d73a6
Rashed Abdel-Tawab <rashed@linux.com>
2018-09-15 09:00:10 -0700

    mata: Update vendor security patch level
    Change-Id: I53c783eb4f99cb143429701de29546f2aec43e69

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -172,7 +172,7 @@ TARGET_RECOVERY_UI_MARGIN_WIDTH := 64
 BOARD_ROOT_EXTRA_FOLDERS := bt_firmware dsp firmware persist
 
 # Security Patch Level
-VENDOR_SECURITY_PATCH := 2018-07-05
+VENDOR_SECURITY_PATCH := 2018-09-05
 
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
