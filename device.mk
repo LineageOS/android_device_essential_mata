0e4ce0351406c2e9959f75cc3fdd4f952278914f
Ethan Chen <intervigil@gmail.com>
2018-12-09 16:10:45 -0800

    mata: Add prebuilt health@2.0 service
    Change-Id: I14cfb9a13048a6011cf7d22cd9ff4ac7526e3fe2

--- device.mk
+++ device.mk
@@ -174,11 +174,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     device/essential/mata/configs/hdr_tm_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/hdr_tm_config.xml
 
-# Health
-PRODUCT_PACKAGES += \
-    android.hardware.health@1.0-impl \
-    android.hardware.health@1.0-service
-
 # IMS
 PRODUCT_PACKAGES += \
     ims-ext-common
