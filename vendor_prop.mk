a0b91576209e94dfefa8a215445dcb36b479f8ba
SafeGuardOS <46631343+SafeGuardOS@users.noreply.github.com>
2019-01-30 21:37:50 -0500

    Update vendor_prop.mk

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -42,8 +42,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
-    #vendor.camera.aux.packagelist="org.codeaurora.snapcam,com.essential.klik,org.lineageos.snap" \
-    vendor.camera.aux.packagelist="org.codeaurora.snapcam,org.lineageos.snap" \
+    vendor.camera.aux.packagelist="org.codeaurora.snapcam,com.essential.klik,org.lineageos.snap" \
+    #vendor.camera.aux.packagelist="org.codeaurora.snapcam,org.lineageos.snap" \
     persist.camera.camera2=true \
     persist.camera.expose.aux=1 \
     persist.camera.is_type=3 \
