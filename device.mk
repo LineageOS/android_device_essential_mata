dca505016e26690ce3c824929b87278abdf9f898
Paul Keith <javelinanddart@gmail.com>
2019-02-22 21:56:53 +0100

    mata: Switch to livedisplay@2.0
    * Build needed services and add supported interfaces to manifest
    
    Change-Id: I0dcccd12a1d355f1e69863183f1165f051b66230

--- device.mk
+++ device.mk
@@ -211,7 +211,8 @@ PRODUCT_PACKAGES += \
 
 # LiveDisplay native
 PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@1.0-service-sdm
+    vendor.lineage.livedisplay@2.0-service-sdm \
+    vendor.lineage.livedisplay@2.0-service-sysfs
 
 # Media
 PRODUCT_COPY_FILES += \
