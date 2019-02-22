3fe5fc97a91db68142ddc7d9ab63a7b77abcb9ed
Ethan Chen <intervigil@gmail.com>
2019-02-22 00:20:59 -0800

    mata: Remove sys.slpi.firmware.version property
    * Nothing reads this
    
    Change-Id: Ieb2e87b369322836504da2b67ce030306f9ebf37

--- rootdir/bin/init.qcom.devstart.sh
+++ rootdir/bin/init.qcom.devstart.sh
@@ -4,8 +4,3 @@ echo 1 > /sys/kernel/boot_adsp/boot
 echo 1 > /sys/kernel/boot_cdsp/boot
 echo 1 > /sys/kernel/boot_slpi/boot
 setprop vendor.qcom.devup 1
-
-version=`grep -ao "OEM_IMAGE_VERSION_STRING[ -~]*" \
-              /firmware/image/slpi_v2.b04 | \
-         sed -e s/OEM_IMAGE_VERSION_STRING=SLPI.version.// -e s/\(.*\).//`
-setprop sys.slpi.firmware.version "$version"
