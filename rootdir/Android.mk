5e815af4bc51879ba61d1acf5ae5c7a758512d89
Ethan Chen <intervigil@gmail.com>
2019-02-28 21:35:05 -0800

    mata: Fold init.mata.hdmi.sh into init.mata.rc
    Change-Id: I93f45d56abd6708117d2410363d07166c5cdc4f9

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -12,14 +12,6 @@ LOCAL_VENDOR_MODULE    := true
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.mata.hdmi.sh
-LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := EXECUTABLES
-LOCAL_SRC_FILES    := bin/init.mata.hdmi.sh
-LOCAL_VENDOR_MODULE    := true
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE       := init.mata.ramdump.rc
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
