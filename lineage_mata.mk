538a057ea11df935b69c172808538362cccb4bba
Bowgo Tsai <bowgotsai@google.com>
2019-04-08 18:28:48 +0800

    mata: Adding GSI keys
    Change-Id: I112ece91566e6cea066cd637bfed5cae6761ae66

--- lineage_mata.mk
+++ lineage_mata.mk
@@ -8,6 +8,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 # Vendor blobs
 $(call inherit-product-if-exists, vendor/essential/mata/mata-vendor.mk)
 
+# Installs gsi keys into ramdisk, to boot a GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
 # Device
 $(call inherit-product, device/essential/mata/device.mk)
 
