5a5b7df83d4b2b76dc206661f37ba0c9e0deb753
Jiyong Park <jiyong@google.com>
2019-04-15 14:19:03 +0900

    mata: Add updatable_apex.mk
    The product abstracts configs that need to be set to support updating of APEXes.
    
    Change-Id: Id5aa944ceba6a6846308f1c18a08af8b3bb38589

--- lineage_mata.mk
+++ lineage_mata.mk
@@ -11,6 +11,9 @@ $(call inherit-product-if-exists, vendor/essential/mata/mata-vendor.mk)
 # Installs gsi keys into ramdisk, to boot a GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
 # Device
 $(call inherit-product, device/essential/mata/device.mk)
 
