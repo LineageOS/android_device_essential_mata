b69ffc3640da3ea2443c53ec93071cdfdf0b7355
Benedict Wong <benedictwong@google.com>
2019-07-16 12:57:20 -0700

    mata: Add feature flag for FEATURE_IPSEC_TUNNELS
    This commit adds the IPSEC_TUNNELS feature flag, with XFRM Interfaces
    used as the implementation of choice.
    
    Bug: 117183273
    Bug: 137629222
    Test: Compiles, passes tunnel-mode CTS tests
    Test: adb shell pm list features
    Change-Id: I19c598947525783850b29d149db85db89f8b347e

--- device.mk
+++ device.mk
@@ -295,6 +295,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.vulkan.version-1_1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.version.xml \
     frameworks/native/data/etc/android.hardware.wifi.direct.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.direct.xml \
     frameworks/native/data/etc/android.hardware.wifi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.xml \
+    frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml \
     frameworks/native/data/etc/android.software.midi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.midi.xml \
     frameworks/native/data/etc/android.software.opengles.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.opengles.deqp.level.xml \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml \
