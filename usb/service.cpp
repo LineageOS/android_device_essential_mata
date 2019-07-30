5c75cff452252066958f051f2a75ee9bd8dcf600
Thomas Shum <jabashque@gmail.com>
2019-07-29 19:26:23 -0700

    Remove (slightly broken) USB Gadget HAL for now
    This is currently causing issues for Android Auto, where the phone can
    see the connected car but Android Auto doesn't start.
    
    This commit consists of the following reverts:
    
    Revert "mata: Allow USB HAL to access dual_role_usb nodes"
    This reverts commit 29e9a2a31c81f3a494880bda3a8f72a2ef54cdc8.
    
    Revert "Add audio_source combinations to usb hal"
    This reverts commit 912db8227c05af0186eaf9af2fb8284e5b78724d.
    
    Revert "Set VID/PID for diag functions"
    This reverts commit 031db1fb302a923ec33a5315139bbc5faf177c5a.
    
    Revert "Initialize booleans"
    This reverts commit 0ec677b9ef561f2a0a55f60decb9b970e9a3d853.
    
    Revert "Usb Gadget hal implementation for mata"
    This reverts commit 841419065a88b247b2485780c6d61a9729e3b18e.
    
    Change-Id: I1981680d2325abc8ea9ad7b04dafbda286e69bb1

--- usb/service.cpp
+++ usb/service.cpp
@@ -17,7 +17,6 @@
 
 #include <hidl/HidlTransportSupport.h>
 #include "Usb.h"
-#include "UsbGadget.h"
 
 using android::sp;
 
@@ -27,36 +26,23 @@ using android::hardware::joinRpcThreadpool;
 
 // Generated HIDL files
 using android::hardware::usb::V1_1::IUsb;
-using android::hardware::usb::gadget::V1_0::IUsbGadget;
 using android::hardware::usb::V1_1::implementation::Usb;
-using android::hardware::usb::gadget::V1_0::implementation::UsbGadget;
 
-using android::OK;
 using android::status_t;
+using android::OK;
 
 int main() {
 
     android::sp<IUsb> service = new Usb();
-    android::sp<IUsbGadget> service2 = new UsbGadget();
 
-    configureRpcThreadpool(2, true /*callerWillJoin*/);
+    configureRpcThreadpool(1, true /*callerWillJoin*/);
     status_t status = service->registerAsService();
 
-    if (status != OK) {
-        ALOGE("Cannot register USB HAL service");
-        return 1;
+    if (status == OK) {
+        ALOGI("USB HAL Ready.");
+        joinRpcThreadpool();
     }
 
-    status = service2->registerAsService();
-
-    if (status != OK) {
-        ALOGE("Cannot register USB Gadget HAL service");
-        return 1;
-    }
-
-    ALOGI("USB HAL Ready.");
-    joinRpcThreadpool();
-    // Under noraml cases, execution will not reach this line.
-    ALOGI("USB HAL failed to join thread pool.");
+    ALOGE("Cannot register USB HAL service");
     return 1;
 }
