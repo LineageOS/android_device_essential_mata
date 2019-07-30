8f83b711f085cd38e80b5770c43ee13271035b72
Nolen Johnson <johnsonnolen@gmail.com>
2019-07-30 02:15:54 +0000

    Revert "mata: Fold init.mata.hdmi.sh into init.mata.rc"
    * This breaks HDMI out, and I see no massive reason to go though
      converting the runtime prop changes correctly.
    
    This reverts commit 5e815af4bc51879ba61d1acf5ae5c7a758512d89.
    
    Change-Id: I2941816dff9239101ca4c3ec9629a799130a6450

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -48,7 +48,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
-    debug.gralloc.enable_fb_ubwc=1 \
     debug.sf.hw=1 \
     dev.pm.dyn_samplingrate=1 \
     ro.opengles.version=196610 \
