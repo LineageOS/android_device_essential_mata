fbc42b1c1eb6084fab632f8b761b96f38f3708ab
Kevin F. Haggerty <haggertk@lineageos.org>
2019-08-05 19:09:05 -0600

    mata: Update fingerprint and security level from PQ1A.190105.112
    * Note: This includes a horri-bad setting of platform security level
      in order to pass a CTS profile match.
    
    Change-Id: I957c27f2300d48b66769dec1727c6636feb460ac

--- lineage_mata.mk
+++ lineage_mata.mk
@@ -28,6 +28,6 @@ PRODUCT_RELEASE_NAME := mata
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
         PRODUCT_NAME=mata \
-        PRIVATE_BUILD_DESC="mata-user 9 PQ1A.190105.090 528 release-keys"
+        PRIVATE_BUILD_DESC="mata-user 9 PQ1A.190105.112 550 release-keys"
 
-BUILD_FINGERPRINT := essential/mata/mata:9/PQ1A.190105.090/528:user/release-keys
+BUILD_FINGERPRINT := essential/mata/mata:9/PQ1A.190105.112/550:user/release-keys
