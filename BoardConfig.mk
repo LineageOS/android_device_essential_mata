e420071845b26631b3ff58006d3884aec5a4a7d2
Jimmy Chen <jimmycmchen@google.com>
2019-10-03 11:24:23 +0800

    mata: Move supplicant service init to wpa_supplicant
    Bug: 138790309
    Test: boot and wifi function is ok
    Change-Id: I35cf3e03566443e20799623045eacd423fd7f5c4

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -143,6 +143,7 @@ WIFI_DRIVER_FW_PATH_STA := "sta"
 WIFI_DRIVER_FW_PATH_AP  := "ap"
 WIFI_DRIVER_FW_PATH_P2P := "p2p"
 WIFI_HIDL_FEATURE_DUAL_INTERFACE := true
+WIFI_HIDL_UNIFIED_SUPPLICANT_SERVICE_RC_ENTRY := true
 
 # Inherit from the proprietary version
 include vendor/essential/mata/BoardConfigVendor.mk
