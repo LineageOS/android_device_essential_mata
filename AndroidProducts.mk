celtare21/android_device_essential_mata-1

mata: Switch from add_lunch_combo to COMMON_LUNCH_CHOICES
Bug: 77599627
Test: lunch  [see entries in list]
Change-Id: Id44fcfec47eeb979a745f879031f1bec7359a633
Signed-off-by: celtare21 <celtare21@gmail.com>

AndroidProducts.mk
@@ -16,3 +16,8 @@
 
 PRODUCT_MAKEFILES += \
     device/essential/mata/potato_mata.mk
+
+COMMON_LUNCH_CHOICES := \
+    potato_mata-user \
+    potato_mata-userdebug \
+    potato_mata-eng

LineageOS/android_device_essential_mata

AndroidProducts.mk
@@ -1,5 +1,5 @@
 #
-# Copyright 2018 The LineageOS Project
+# Copyright (C) 2018-2019 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,3 +16,8 @@
 
 PRODUCT_MAKEFILES += \
     device/essential/mata/lineage_mata.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_mata-user \
+    lineage_mata-userdebug \
+    lineage_mata-eng