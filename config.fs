1bcfa9904e18a249d5688a028c794fb4a34f8aab
Ethan Chen <intervigil@gmail.com>
2019-10-18 23:19:20 -0700

    mata: Remove firmware dirs from config.fs
    Change-Id: I68a542994790cea244d18b6244224a4faee6ba56

--- config.fs
+++ config.fs
@@ -132,27 +132,3 @@ mode: 0755
 user:  AID_GPS
 group: AID_GPS
 caps: NET_BIND_SERVICE BLOCK_SUSPEND
-
-[firmware/]
-mode: 0771
-user: AID_SYSTEM
-group: AID_SYSTEM
-caps: 0
-
-[bt_firmware/]
-mode: 0771
-user: AID_SYSTEM
-group: AID_SYSTEM
-caps: 0
-
-[persist/]
-mode: 0771
-user: AID_SYSTEM
-group: AID_SYSTEM
-caps: 0
-
-[dsp/]
-mode: 0771
-user: AID_MEDIA
-group: AID_MEDIA
-caps: 0
