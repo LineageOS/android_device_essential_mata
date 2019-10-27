df7e559232db7818f3e91f875730c2594145a114
Ethan Chen <intervigil@gmail.com>
2019-10-27 13:58:44 -0700

    mata: Use sepolicy-legacy-um SELinux policy
    Change-Id: I024e801a1be625b8b83205bbaafe0d4297499978

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -186,7 +186,7 @@ BOARD_ROOT_EXTRA_SYMLINKS += /vendor/firmware_mnt:/firmware
 VENDOR_SECURITY_PATCH := 2019-10-05
 
 # SELinux
-include device/qcom/sepolicy/sepolicy.mk
+include device/qcom/sepolicy-legacy-um/sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
