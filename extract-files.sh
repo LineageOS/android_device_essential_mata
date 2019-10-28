4c5b6a581c08948545180b960be5cf841b823d84
Ethan Chen <intervigil@gmail.com>
2019-10-28 00:11:33 -0700

    mata: Remove 32-bit IMS libs
    Change-Id: I926b320629fa4c04971569a3ef53c7a9bf9e95d7

--- extract-files.sh
+++ extract-files.sh
@@ -64,7 +64,5 @@ sed -i 's/service sidecar-hal-1-0/service vendor.sidecar-hal-1-0/g' "$LINEAGE_RO
 patchelf --add-needed "libbase_shim.so" "$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary/vendor/bin/imsrcsd
 patchelf --add-needed "libbase_shim.so" "$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary/vendor/lib64/lib-imsrcs-v2.so
 patchelf --add-needed "libbase_shim.so" "$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary/vendor/lib64/lib-uceservice.so
-patchelf --add-needed "libbase_shim.so" "$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary/vendor/lib/lib-imsrcs-v2.so
-patchelf --add-needed "libbase_shim.so" "$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary/vendor/lib/lib-uceservice.so
 
 "$MY_DIR"/setup-makefiles.sh
