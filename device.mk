13d4ff2a51d7b9af874a22d44142871a6f7178ad
Robert Shih <robertshih@google.com>
2019-11-25 22:25:41 -0800

    mata: Enable v1.3 drm plugins
    Bug: 139134043
    Test: MediaDrmTest
    Change-Id: I7453b035ca29c7ee889d03de0d55e6d947b3cb77

--- device.mk
+++ device.mk
@@ -149,7 +149,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
-    android.hardware.drm@1.2-service.clearkey \
+    android.hardware.drm@1.3-service.clearkey \
     move_widevine_data.sh
 
 # Fingerprint
