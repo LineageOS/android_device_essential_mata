4cb0b694850d11bbb8dcdc18a9957c0ef3759c1d
Ethan Chen <intervigil@gmail.com>
2020-04-11 19:14:54 -0700

    mata: Import Essential vibrator source
    * Essential revision: 1f9551e4756f856716a3872375a6ba1c730eb6dd
    
    Change-Id: I4a40dea3c7b7cd7c1f03bcdea72a5c03fbccf35e

--- vibrator/Vibrator.cpp
+++ vibrator/Vibrator.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017 The LineageOS Project
+ * Copyright (C) 2017 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,126 +18,37 @@
 
 #include <log/log.h>
 
+#include <hardware/hardware.h>
+#include <hardware/vibrator.h>
 #include <cutils/properties.h>
 
 #include "Vibrator.h"
 
+#include <cinttypes>
 #include <cmath>
+#include <iostream>
+#include <fstream>
 
-#define ARRAY_SIZE(x) (sizeof(x)/sizeof(x[0]))
-
-namespace {
-static constexpr int32_t MIN_VTG_INPUT = 120;
-static constexpr int32_t MAX_VTG_INPUT = 2750;
-
-static constexpr char MODE_DIRECT[] = "direct";
-static constexpr char MODE_BUFFER[] = "buffer";
-
-static constexpr int32_t WAVEFORM_CLICK_EFFECT_MS = 16;
-static constexpr uint8_t WAVEFORM_CLICK_EFFECT_SEQ[] = {
-    18, 24, 26, 40, 62, 0, 0, 0
-};
-
-static constexpr int32_t WAVEFORM_TICK_EFFECT_MS = 8;
-static constexpr uint8_t WAVEFORM_TICK_EFFECT_SEQ[] = {
-    18, 40, 62, 40, 16, 4, 0, 0
-};
-
-static constexpr uint32_t WAVEFORM_DOUBLE_CLICK_EFFECT_MS = 128;
-static constexpr uint8_t WAVEFORM_DOUBLE_CLICK_EFFECT_SEQ[] = {
-    62, 48, 24, 0, 0, 62, 48, 24
-};
-
-/*
- * Vibrator buffer mode
- *
- * 8 byte buffer, corresponding to a whole buffer played at a time
- * Each byte is a voltage value, corresponding to the following
- * formula:
- * Voltage = ((0.116mV * (VALUE >> 1))
- * Voltage = Voltage * 2 if (0x40 & VALUE)
- * Each byte plays for period set in DTSI, qcom,play-rate-us, in microseconds
- *
- * Valid values of VALUE without overdrive (x2 voltage) range from
- * 0 to 31, for valid voltages of 0-3596
- *
- * Valid values of VALUE with overdrive (x2 voltage) range from
- * 32 to 62, for valid voltages of 3712-7192
- */
-static int convertVoltageLevel(uint8_t voltageLevel) {
-    uint8_t val = 0;
-
-    if (voltageLevel > 62) {
-        return -EINVAL;
-    }
-
-    val = voltageLevel;
-
-    if (voltageLevel > 31) {
-        /* Divide value by 2, since setting bit 6 doubles value */
-        val = val >> 1;
-    }
-
-    /* Shift one bit, since bit 0 is unused */
-    val = val << 1;
-
-    /* Set bit 6 for overdrive */
-    if (voltageLevel > 31) {
-        val |= 0x40;
-    }
-
-    return val;
-}
-
-static int convertEffectStrength(
-        android::hardware::vibrator::V1_0::EffectStrength strength,
-        int voltageLevel) {
-    switch (strength) {
-    case android::hardware::vibrator::V1_0::EffectStrength::LIGHT:
-        /* Cut strength in half */
-        return voltageLevel >> 1;
-        break;
-    case android::hardware::vibrator::V1_0::EffectStrength::MEDIUM:
-    case android::hardware::vibrator::V1_0::EffectStrength::STRONG:
-    default:
-        return voltageLevel;
-        break;
-    }
-}
-} // anonymous namespace
 
 namespace android {
 namespace hardware {
 namespace vibrator {
-namespace V1_1 {
+namespace V1_2 {
 namespace implementation {
 
+static constexpr int32_t WAVEFORM_CLICK_EFFECT_MS = 6;
+
 using Status = ::android::hardware::vibrator::V1_0::Status;
+using EffectStrength = ::android::hardware::vibrator::V1_0::EffectStrength;
 
-Vibrator::Vibrator(std::ofstream&& duration, std::ofstream&& vtgInput,
-        std::ofstream&& mode, std::ofstream&& bufferUpdate,
-        std::vector<std::ofstream>&& buffers) :
-    mDuration(std::move(duration)),
-    mVtgInput(std::move(vtgInput)),
-    mMode(std::move(mode)),
-    mBufferUpdate(std::move(bufferUpdate)),
-    mBuffers(std::move(buffers)) {
+Vibrator::Vibrator(std::ofstream&& activate, std::ofstream&& scale) :
+    mActivate(std::move(activate)),
+    mScale(std::move(scale)) {}
 
-    mClickDuration = property_get_int32("ro.vibrator.hal.click.duration",
-            WAVEFORM_CLICK_EFFECT_MS);
-    mTickDuration = property_get_int32("ro.vibrator.hal.tick.duration",
-            WAVEFORM_TICK_EFFECT_MS);
-}
-
-Return<Status> Vibrator::on(uint32_t timeoutMs, bool isWaveform) {
-    if (isWaveform) {
-        mMode << MODE_BUFFER << std::endl;
-    } else {
-        mMode << MODE_DIRECT << std::endl;
-    }
-
-    mDuration << timeoutMs << std::endl;
-    if (!mDuration) {
+// Methods from ::android::hardware::vibrator::V1_2::IVibrator follow.
+Return<Status> Vibrator::on(uint32_t timeoutMs) {
+    mActivate << timeoutMs << std::endl;
+    if (!mActivate) {
         ALOGE("Failed to activate (%d): %s", errno, strerror(errno));
         return Status::UNKNOWN_ERROR;
     }
@@ -145,104 +56,78 @@ Return<Status> Vibrator::on(uint32_t timeoutMs, bool isWaveform) {
    return Status::OK;
 }
 
-Return<Status> Vibrator::on(uint32_t timeoutMs) {
-    return on(timeoutMs, false /* isWaveform */);
-}
-
 Return<Status> Vibrator::off()  {
-    mDuration << 0 << std::endl;
-    if (!mDuration) {
+    mActivate << 0 << std::endl;
+    if (!mActivate) {
         ALOGE("Failed to turn vibrator off (%d): %s", errno, strerror(errno));
         return Status::UNKNOWN_ERROR;
     }
-
     return Status::OK;
 }
 
 Return<bool> Vibrator::supportsAmplitudeControl()  {
-    return (mVtgInput ? true : false);
+    return false;
 }
 
-Return<Status> Vibrator::setAmplitude(uint8_t amplitude) {
-
-    if (amplitude == 0) {
-        return Status::BAD_VALUE;
-    }
-
-    int32_t voltage =
-            std::lround((amplitude - 1) / 254.0 * (MAX_VTG_INPUT - MIN_VTG_INPUT) +
-            MIN_VTG_INPUT);
-
-    mVtgInput << voltage << std::endl;
-    if (!mVtgInput) {
-        ALOGE("Failed to set amplitude (%d): %s", errno, strerror(errno));
-        return Status::UNKNOWN_ERROR;
-    }
-
-    return Status::OK;
+Return<Status> Vibrator::setAmplitude(uint8_t /*amplitude*/) {
+    return Status::UNSUPPORTED_OPERATION;
 }
 
-Return<void> Vibrator::perform(Effect effect, EffectStrength strength,
+Return<void> Vibrator::perform(V1_0::Effect effect, EffectStrength strength, perform_cb _hidl_cb) {
+    return performEffect(static_cast<Effect>(effect), strength, _hidl_cb);
+}
+
+Return<void> Vibrator::perform_1_1(V1_1::Effect_1_1 /*effect*/, EffectStrength /*strength*/,
         perform_cb _hidl_cb) {
+     _hidl_cb(Status::UNSUPPORTED_OPERATION, 0);
+     return Void();
+}
+
+Return<void> Vibrator::perform_1_2(Effect /*effect*/, EffectStrength /*strength*/, perform_cb _hidl_cb) {
+     _hidl_cb(Status::UNSUPPORTED_OPERATION, 0);
+     return Void();
+}
+
+static uint16_t convertEffectStrength(EffectStrength strength) {
+    uint16_t scale;
+
+    switch (strength) {
+    case EffectStrength::LIGHT:
+        scale = 1375; // 50%
+        break;
+    case EffectStrength::MEDIUM:
+        scale = 2062; // 75%
+        break;
+    case EffectStrength::STRONG:
+        scale = 2750;
+        break;
+    }
+
+    return scale;
+}
+
+Return<void> Vibrator::performEffect(Effect effect, EffectStrength strength, perform_cb _hidl_cb) {
     Status status = Status::OK;
     uint32_t timeMS;
 
-    if (effect == Effect::CLICK) {
-        for (uint32_t i = 0; i < ARRAY_SIZE(WAVEFORM_CLICK_EFFECT_SEQ); i++) {
-            mBuffers[i] << std::hex << convertEffectStrength(strength,
-                    convertVoltageLevel(WAVEFORM_CLICK_EFFECT_SEQ[i])) <<
-                std::endl;
-        }
-        mBufferUpdate << 1 << std::endl;
-        timeMS = mClickDuration;
-    } else if (effect == Effect::DOUBLE_CLICK) {
-        for (uint32_t i = 0; i < ARRAY_SIZE(WAVEFORM_DOUBLE_CLICK_EFFECT_SEQ);
-                i++) {
-            mBuffers[i] << std::hex << convertEffectStrength(strength,
-                    convertVoltageLevel(WAVEFORM_DOUBLE_CLICK_EFFECT_SEQ[i])) <<
-                std::endl;
-        }
-        mBufferUpdate << 1 << std::endl;
-        timeMS = WAVEFORM_DOUBLE_CLICK_EFFECT_MS;
-    } else {
+    switch (effect) {
+    case Effect::CLICK:
+        timeMS = WAVEFORM_CLICK_EFFECT_MS;
+        break;
+    default:
         _hidl_cb(Status::UNSUPPORTED_OPERATION, 0);
         return Void();
     }
 
-    on(timeMS, true /* isWaveform */);
-
+    mScale << convertEffectStrength(strength) << std::endl;
+    on(timeMS);
     _hidl_cb(status, timeMS);
     return Void();
 }
 
-Return<void> Vibrator::perform_1_1(Effect_1_1 effect, EffectStrength strength,
-        perform_cb _hidl_cb) {
-    Status status = Status::OK;
-    uint32_t timeMS;
-
-    if (effect == Effect_1_1::TICK) {
-        for (uint32_t i = 0; i < ARRAY_SIZE(WAVEFORM_TICK_EFFECT_SEQ); i++) {
-            mBuffers[i] << std::hex << convertEffectStrength(strength,
-                    convertVoltageLevel(WAVEFORM_TICK_EFFECT_SEQ[i])) <<
-                std::endl;
-        }
-        mBufferUpdate << 1 << std::endl;
-        timeMS = mTickDuration;
-    } else if (effect < Effect_1_1::TICK) {
-        return perform(static_cast<Effect>(effect), strength, _hidl_cb);
-    } else {
-        _hidl_cb(Status::UNSUPPORTED_OPERATION, 0);
-        return Void();
-    }
-
-    on(timeMS, true /* isWaveform */);
-
-    _hidl_cb(status, timeMS);
-    return Void();
-}
 
 } // namespace implementation
-} // namespace V1_1
-} // namespace vibrator
-} // namespace hardware
-} // namespace android
+}  // namespace V1_2
+}  // namespace vibrator
+}  // namespace hardware
+}  // namespace android
--- vibrator/Vibrator.h
+++ vibrator/Vibrator.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017 The LineageOS Project
+ * Copyright (C) 2017 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,53 +13,47 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#pragma once
+#ifndef ANDROID_HARDWARE_VIBRATOR_V1_2_VIBRATOR_H
+#define ANDROID_HARDWARE_VIBRATOR_V1_2_VIBRATOR_H
 
-#include <android/hardware/vibrator/1.1/IVibrator.h>
+#include <android/hardware/vibrator/1.2/IVibrator.h>
 #include <hidl/Status.h>
 
 #include <fstream>
-#include <vector>
 
 namespace android {
 namespace hardware {
 namespace vibrator {
-namespace V1_1 {
+namespace V1_2 {
 namespace implementation {
 
 class Vibrator : public IVibrator {
 public:
-    Vibrator(std::ofstream&& duration, std::ofstream&& vtgInput,
-            std::ofstream&& mode, std::ofstream&& bufferUpdate,
-            std::vector<std::ofstream>&& buffers);
+    Vibrator(std::ofstream&& activate, std::ofstream&& scale);
 
     // Methods from ::android::hardware::vibrator::V1_0::IVibrator follow.
     using Status = ::android::hardware::vibrator::V1_0::Status;
-    Return<Status> on(uint32_t timeoutMs) override;
-    Return<Status> off() override;
+    Return<Status> on(uint32_t timeoutMs)  override;
+    Return<Status> off()  override;
     Return<bool> supportsAmplitudeControl() override;
     Return<Status> setAmplitude(uint8_t amplitude) override;
 
     using EffectStrength = ::android::hardware::vibrator::V1_0::EffectStrength;
-    using Effect = ::android::hardware::vibrator::V1_0::Effect;
-    Return<void> perform(Effect effect, EffectStrength strength,
-            perform_cb _hidl_cb) override;
-    Return<void> perform_1_1(Effect_1_1 effect, EffectStrength strength,
-            perform_cb _hidl_cb) override;
+    Return<void> perform(V1_0::Effect effect, EffectStrength strength, perform_cb _hidl_cb)
+            override;
+    Return<void> perform_1_1(V1_1::Effect_1_1 effect, EffectStrength strength, perform_cb _hidl_cb)
+            override;
+    Return<void> perform_1_2(Effect effect, EffectStrength strength, perform_cb _hidl_cb) override;
 
 private:
-    Return<Status> on(uint32_t timeoutMs, bool isWaveform);
-    std::ofstream mDuration;
-    std::ofstream mVtgInput;
-    std::ofstream mMode;
-    std::ofstream mBufferUpdate;
-    std::vector<std::ofstream> mBuffers;
-    int32_t mClickDuration;
-    int32_t mTickDuration;
+    Return<void> performEffect(Effect effect, EffectStrength strength, perform_cb _hidl_cb);
+    std::ofstream mActivate;
+    std::ofstream mScale;
 };
-
 }  // namespace implementation
-}  // namespace V1_0
+}  // namespace V1_2
 }  // namespace vibrator
 }  // namespace hardware
 }  // namespace android
+
+#endif  // ANDROID_HARDWARE_VIBRATOR_V1_2_VIBRATOR_H
