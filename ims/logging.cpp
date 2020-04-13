80b5a223b8c8d5b760860db39621c3f099d2fca6
Ethan Chen <intervigil@gmail.com>
2020-04-12 17:15:54 -0700

    mata: Fix libbase shim constructor
    * Provide a class object pointer parameter for constructor shim
      functions.
    
    Change-Id: I431d12b9d6b2da2b3febb52d62fb321996921b16

--- ims/logging.cpp
+++ ims/logging.cpp
@@ -1,12 +1,22 @@
 #include <android-base/logging.h>
 
 extern "C" void _ZN7android4base10LogMessageC1EPKcjNS0_5LogIdENS0_11LogSeverityES3_i(
-        const char* file, unsigned int line, android::base::LogId id,
-        android::base::LogSeverity severity, const char* tag, int error);
+        void* logMessage, const char* file, unsigned int line,
+        android::base::LogId id, android::base::LogSeverity severity,
+        const char* tag, int error);
 
 extern "C" void _ZN7android4base10LogMessageC1EPKcjNS0_5LogIdENS0_11LogSeverityEi(
-        const char* file, unsigned int line, android::base::LogId id,
-        android::base::LogSeverity severity, int error) {
-    _ZN7android4base10LogMessageC1EPKcjNS0_5LogIdENS0_11LogSeverityES3_i(file,
-            line, id, severity, nullptr, error);
+        void* logMessage, const char* file, unsigned int line,
+        android::base::LogId id, android::base::LogSeverity severity,
+        int error) {
+    _ZN7android4base10LogMessageC1EPKcjNS0_5LogIdENS0_11LogSeverityES3_i(
+            logMessage, file, line, id, severity, nullptr, error);
+}
+
+extern "C" void _ZN7android4base10LogMessageC2EPKcjNS0_5LogIdENS0_11LogSeverityEi(
+        void* logMessage, const char* file, unsigned int line,
+        android::base::LogId id, android::base::LogSeverity severity,
+        int error) {
+    _ZN7android4base10LogMessageC1EPKcjNS0_5LogIdENS0_11LogSeverityES3_i(
+            logMessage, file, line, id, severity, nullptr, error);
 }
