6599339fd693045a5a9aa22c1f8099f7dc7791d3
Ethan Chen <intervigil@gmail.com>
2020-04-12 22:23:29 -0700

    mata: Add audio_output_policy.conf
    * Update for LA.UM.8.4 HAL
    
    Change-Id: I9a299a8225dd1b27b552f00bbc792f892dada528

--- device.mk
+++ device.mk
@@ -52,6 +52,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     device/essential/mata/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
+    device/essential/mata/audio/audio_output_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_output_policy.conf \
     device/essential/mata/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
     device/essential/mata/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
     device/essential/mata/audio/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
