1ea904ffdfd0eda1302309b16b15e6dccce0dd5f
David Sehr <sehr@google.com>
2020-04-25 15:50:40 -0700

    mata: Use 64bit dex2oat on all cores
    Bug: 153380900
    Test: boot and install an application
    Change-Id: I3e7a6e6e9385ff6564d1a2e6dda004ebb061f095

--- system_prop.mk
+++ system_prop.mk
@@ -19,7 +19,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.heapsize=512m \
     dalvik.vm.heaptargetutilization=0.75 \
     dalvik.vm.heapminfree=512k \
-    dalvik.vm.heapmaxfree=8m
+    dalvik.vm.heapmaxfree=8m \
+    dalvik.vm.dex2oat64.enabled=true \
+    ro.sys.fw.dex2oat_thread_count=8
 
 # Logging
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
