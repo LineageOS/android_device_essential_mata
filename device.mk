61d62e90f764f848151e9d5b4de72dcc2b00c1fb
Joshua Blanchard <joshua.lee.bbg@gmail.com>
2020-05-11 21:14:40 -0400

    mata: Upver NFC hal to version 1.2
    Change-Id: I5a0a04c3d103d3f1292df95395e8f74138b1a18c
    Signed-off-by: Joshua Blanchard <joshua.lee.bbg@gmail.com>

--- device.mk
+++ device.mk
@@ -236,7 +236,7 @@ PRODUCT_PACKAGES += \
 
 # NFC
 PRODUCT_PACKAGES += \
-    android.hardware.nfc@1.1-service \
+    android.hardware.nfc@1.2-service \
     com.android.nfc_extras \
     NfcNci \
     Tag
