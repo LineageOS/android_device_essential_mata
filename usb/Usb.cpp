a5008d4fd2e0539e58be1ed247d40a11cc32a99f
Ethan Chen <intervigil@gmail.com>
2020-05-30 00:34:45 -0700

    Revert "mata: Remove USB Gadget HAL for now"
    * Use the USB gadget driver.
    
    This reverts commit 71905b226ebaa8778b7a4d5803fa38985168af58.
    
    Change-Id: I4a08f6b862bfe963d353c23c71db986c7bc0506a

--- usb/Usb.cpp
+++ usb/Usb.cpp
@@ -106,7 +106,7 @@ std::string convertRoletoString(PortRole role) {
 }
 
 Return<void> Usb::switchRole(const hidl_string& portName,
-        const PortRole& newRole) {
+        const V1_0::PortRole& newRole) {
     std::string filename = appendRoleNodeHelper(std::string(portName.c_str()),
         newRole.type);
     std::ofstream file(filename);
