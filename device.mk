48728bbbc542d5c9d869a0755e6bdfff46bf4032
Edwin Wong <edwinwong@google.com>
2021-03-25 14:36:24 -0700

    mata: Update clearkey plugin to drm HAL v1.4
    Test: atest
      atest CtsMediaTestCases:android.media.cts.MediaDrmClearkeyTest#testGetPlaybackComponent
    
    Bug: 182525516
    Change-Id: I406b7d3c2921d4698535421894c6eb3785eb1c0d

--- device.mk
+++ device.mk
@@ -133,7 +133,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
-    android.hardware.drm@1.3-service.clearkey
+    android.hardware.drm@1.4-service.clearkey
 
 # Fingerprint
 PRODUCT_PACKAGES += \
