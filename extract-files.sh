167c643168df84d2659235e68e35625d3c82cddc
Nolen Johnson <johnsonnolen@gmail.com>
2021-04-06 12:05:39 -0400

    mata: Drop recovery proprietary files
    * Touch hasn't worked in ages.
    
    Change-Id: I6c2e7d11d3cf4d5522065419ee9ac3c2567d4e13

--- extract-files.sh
+++ extract-files.sh
@@ -83,6 +83,5 @@ function blob_fixup() {
 setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
 
 extract "${MY_DIR}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
-extract "${MY_DIR}/proprietary-files-recovery.txt" "${SRC}" "${KANG}" --section "${SECTION}"
 
 "${MY_DIR}/setup-makefiles.sh"
