0a3da7e518db137a1bb4b83f3705a9075ba7ab45
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-06-20 02:49:03 +0200

    mata: libqti-perfd-client: Move to C
    Why bother with C++ mangling if we can build it as standard C library?
    
    Change-Id: I45ea977edf7ea7fab6fece76f3049654a8d24c5d

--- libqti-perfd-client/client.c
+++ libqti-perfd-client/client.c
@@ -9,9 +9,9 @@
 #include <stdint.h>
 #include <log/log.h>
 
-extern "C" void perf_get_feedback() {}
-extern "C" void perf_hint() {}
-extern "C" int perf_lock_acq(int handle, int duration, int arg3[], int arg4) {
+void perf_get_feedback() {}
+void perf_hint() {}
+int perf_lock_acq(int handle, int duration, int arg3[], int arg4) {
     ALOGI("perf_lock_acq: handle: %d, duration: %d, arg3[0]: %d, arg4: %d",
             handle, duration, arg3[0], arg4);
     if (handle > 0)
@@ -19,12 +19,12 @@ extern "C" int perf_lock_acq(int handle, int duration, int arg3[], int arg4) {
 
     return 233;
 }
-extern "C" void perf_lock_cmd() {}
-extern "C" int perf_lock_rel(int handle) {
+void perf_lock_cmd() {}
+int perf_lock_rel(int handle) {
     ALOGI("perf_lock_rel: handle: %d", handle);
     if (handle > 0)
         return handle;
 
     return 233;
 }
-extern "C" void perf_lock_use_profile() {}
+void perf_lock_use_profile() {}
