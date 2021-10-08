8bf988906da2647bfb0a1407fb3a9b947a0ca51b
Roberto Sartori <roberto.sartori.android@gmail.com>
2021-10-08 23:57:27 +0200

    mata: Disable compressed APEX
    Compressed APEX has been introduced in Android 12, and they are enabled
    by default inheriting updatable_apex.mk.
    Unfortunately, when the device is encrypted, they are causing bootloops
    on msm8998.
    As the only advantage is some space gain on disk, we can
    simply disable them: keep the variable immediately after including the
    updatable_apex.mk to avoid being overwritten.
    
    Signed-off-by: Roberto Sartori <roberto.sartori.android@gmail.com>
    Change-Id: If46fa28e4bc59af80de441d1fca6c3f13f395168

--- lineage_mata.mk
+++ lineage_mata.mk
@@ -11,6 +11,10 @@ $(call inherit-product, vendor/essential/mata/mata-vendor.mk)
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
+# Disable APEX compression
+# Keep this after including updatable_apex.mk
+PRODUCT_COMPRESSED_APEX := false
+
 # Device
 $(call inherit-product, device/essential/mata/device.mk)
 
