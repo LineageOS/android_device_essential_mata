25c9654ecf9013bbe1ec19b2ba44095581e63bf9
Tommaso Fonda <fondatommaso2@gmail.com>
2022-03-07 12:14:46 +0100

    mata: implement touchscreen support in recovery
        * The adsp library search path includes /system/lib and /system/vendor/lib,
          but not /vendor/lib. Since in recovery the /system/vendor link to /vendor
          does not exist, we must place the rfsa/adsp/ libraries in /system for them
          to be loaded correctly.
    
    Change-Id: I5d3ca280317f7983581cc6882508759a84f6f0ab

--- extract-files.sh
+++ extract-files.sh
@@ -83,5 +83,6 @@ function blob_fixup() {
 setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
 
 extract "${MY_DIR}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
+extract "${MY_DIR}/proprietary-files-recovery.txt" "${SRC}" "${KANG}" --section "${SECTION}"
 
 "${MY_DIR}/setup-makefiles.sh"
