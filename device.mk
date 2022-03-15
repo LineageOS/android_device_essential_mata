563c6539363dbe6e2aae31ba26c9db2d8321f0d7
Bruno Martins <bgcngm@gmail.com>
2022-03-15 20:53:47 +0000

    mata: Copy cgroups and task_profiles directly from system/core
    This ensures that the most up-to-date JSON files are shipped.
    
    Change-Id: Ic9fae188784825ae6dc607c13b403e37290b7067

--- device.mk
+++ device.mk
@@ -110,6 +110,11 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service \
     libxml2
 
+# Cgroup and task_profiles
+PRODUCT_COPY_FILES += \
+    system/core/libprocessgroup/profiles/cgroups_28.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
+    system/core/libprocessgroup/profiles/task_profiles_28.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
+
 # Device settings
 PRODUCT_PACKAGES += \
     DeviceSettings
