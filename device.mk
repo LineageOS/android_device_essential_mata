a3c9a6fd50004fc044b9c2d4f9f07f718cd11c7b
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-05-28 10:29:35 +0200

    mata: Stop explicitly building netutils-wrapper-1.0
    * Built by default
    
    Change-Id: Id6666535bb8d0b84d2d0a8d19411fafa49df3d68

--- device.mk
+++ device.mk
@@ -240,10 +240,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.system.net.netd@1.1.vendor
 
-# Network
-PRODUCT_PACKAGES += \
-    netutils-wrapper-1.0
-
 # NFC
 PRODUCT_PACKAGES += \
     android.hardware.nfc@1.2-service \
