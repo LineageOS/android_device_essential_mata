736171887fe41bb24e0127c239b7b7b5c7ba2eff
Bruno Martins <bgcngm@gmail.com>
2022-05-30 19:27:51 +0100

    mata: Drop default DRM service
    This is not needed, since Widevine DRM and Clearkey DRM
    services are available.
    
    Change-Id: I97023f1bab88ddb8fa4cb205df190c516a759951

--- device.mk
+++ device.mk
@@ -136,8 +136,6 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-impl \
-    android.hardware.drm@1.0-service \
     android.hardware.drm@1.3.vendor \
     android.hardware.drm@1.4-service.clearkey
 
