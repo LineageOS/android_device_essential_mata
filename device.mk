3560676df1d152cab7df2d87f536b499c6c3af23
Tommaso Fonda <fondatommaso2@gmail.com>
2023-10-31 17:18:23 +0100

    mata: Drop unneeded android.hardware.radio.deprecated@1.0.vendor
    Change-Id: I965515092841871db6fade7962327b8919383227

--- device.mk
+++ device.mk
@@ -350,7 +350,6 @@ PRODUCT_PACKAGES += \
     android.hardware.radio@1.1 \
     android.hardware.radio@1.6.vendor \
     android.hardware.radio.config@1.3.vendor \
-    android.hardware.radio.deprecated@1.0.vendor \
     android.hardware.secure_element@1.2.vendor
 
 # Seccomp
