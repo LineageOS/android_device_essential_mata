270bf7b4d6d24f2ec3b3ad33a168509357f78fd2
LuK1337 <priv.luk@gmail.com>
2023-11-09 18:10:51 +0100

    mata: Get rid of AVB 1.0
    It's dead in U.
    
    Change-Id: Id948fe50fd358837cafa3b1e18cf2adba32b927b

--- device.mk
+++ device.mk
@@ -417,11 +417,6 @@ PRODUCT_PACKAGES_DEBUG += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
 
-# Verity
-PRODUCT_SYSTEM_VERITY_PARTITION := /dev/block/platform/soc/1da4000.ufshc/by-name/system
-PRODUCT_VENDOR_VERITY_PARTITION := /dev/block/platform/soc/1da4000.ufshc/by-name/vendor
-$(call inherit-product, build/target/product/verity.mk)
-
 # Vibrator
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.vibrator.service
