a4e44c539b1b1358401a574655e2cbee5241339a
LuK1337 <priv.luk@gmail.com>
2023-11-12 11:17:06 +0100

    mata: Build libprotobuf-cpp-lite-3.9.1-vendorcompat
    Change-Id: I0aac742db9c2b4b3e48ee20158f19405863893c2

--- device.mk
+++ device.mk
@@ -354,7 +354,8 @@ PRODUCT_PACKAGES += \
     android.hardware.radio@1.1 \
     android.hardware.radio@1.6.vendor \
     android.hardware.radio.config@1.3.vendor \
-    android.hardware.secure_element@1.2.vendor
+    android.hardware.secure_element@1.2.vendor \
+    libprotobuf-cpp-lite-3.9.1-vendorcompat
 
 # Seccomp
 PRODUCT_COPY_FILES += \
