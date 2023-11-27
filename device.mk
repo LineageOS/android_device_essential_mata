02c083da3f0de5800c753867bcd8bf39c9f58ad4
Bruno Martins <bgcngm@gmail.com>
2023-11-27 11:33:14 +0100

    mata: Use libhidlbase-v32 for select Android P blobs
    To workaround the removal of gBn/sConstructorMap from libhidl just for
    the blobs that call _ZN7android8hardware7details17gBnConstructorMapE
    and/or _ZN7android8hardware7details17gBsConstructorMapE.
    
    List of blobs referencing the removed symbols:
    
    * lib64/vendor.qti.imsrtpservice@1.0.so
    * recovery/root/vendor/lib64/vendor.qti.hardware.improvetouch.blobmanager@1.0_vendor.so
    * recovery/root/vendor/lib64/vendor.qti.hardware.improvetouch.gesturemanager@1.0_vendor.so
    * recovery/root/vendor/lib64/vendor.qti.hardware.improvetouch.touchcompanion@1.0_vendor.so
    * vendor/lib/vendor.qti.hardware.improvetouch.blobmanager@1.0_vendor.so
    * vendor/lib/vendor.qti.hardware.improvetouch.gesturemanager@1.0_vendor.so
    * vendor/lib/vendor.qti.hardware.improvetouch.touchcompanion@1.0_vendor.so
    * vendor/lib/com.quicinc.cne.api@1.0.so
    * vendor/lib/com.quicinc.cne.server@1.0.so
    * vendor/lib/com.quicinc.cne.server@2.0.so
    * vendor/lib64/com.quicinc.cne.api@1.0.so
    * vendor/lib64/com.quicinc.cne.server@1.0.so
    * vendor/lib64/lib-imsrcs-v2.so
    * vendor/lib64/lib-uceservice.so
    * vendor/lib64/vendor.qti.hardware.improvetouch.blobmanager@1.0_vendor.so
    * vendor/lib64/vendor.qti.hardware.improvetouch.gesturemanager@1.0_vendor.so
    * vendor/lib64/vendor.qti.hardware.improvetouch.touchcompanion@1.0_vendor.so
    * vendor/lib64/vendor.qti.imsrtpservice@1.0_vendor.so
    * vendor/lib64/com.quicinc.cne.server@2.0.so
    
    List of executables that need patching as well, to fix all errors:
    
    * vendor/bin/cnd
    * vendor/bin/hbtp_daemon
    * vendor/bin/ims_rtp_daemon
    
    Change-Id: I2376734cefb9d63775bd4e82feca9c588cd191ac

--- device.mk
+++ device.mk
@@ -420,6 +420,12 @@ PRODUCT_PACKAGES += \
 
 # VNDK
 PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v32/arm64/arch-arm-armv8-a/shared/vndk-sp/libhidlbase.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libhidlbase-v32.so \
+    prebuilts/vndk/v32/arm64/arch-arm-armv8-a/shared/vndk-sp/libhidlbase.so:$(TARGET_COPY_OUT_SYSTEM_EXT)/lib/libhidlbase-v32.so \
+    prebuilts/vndk/v32/arm64/arch-arm-armv8-a/shared/vndk-sp/libhidlbase.so:$(TARGET_COPY_OUT_VENDOR)/lib/libhidlbase-v32.so \
+    prebuilts/vndk/v32/arm64/arch-arm64-armv8-a/shared/vndk-sp/libhidlbase.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libhidlbase-v32.so \
+    prebuilts/vndk/v32/arm64/arch-arm64-armv8-a/shared/vndk-sp/libhidlbase.so:$(TARGET_COPY_OUT_SYSTEM_EXT)/lib64/libhidlbase-v32.so \
+    prebuilts/vndk/v32/arm64/arch-arm64-armv8-a/shared/vndk-sp/libhidlbase.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libhidlbase-v32.so \
     prebuilts/vndk/v33/arm64/arch-arm64-armv8-a/shared/vndk-sp/libutils.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libutils-v33.so
 
 # Update this list with what each blob is actually for
