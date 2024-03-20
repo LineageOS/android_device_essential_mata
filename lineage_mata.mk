7340cbf29661b572a6934f208c97dce0a7a05a48
basamaryan <basam.aryan@gmail.com>
2024-03-20 01:16:12 -0400

    mata: Don't explicitly include updatable_apex.mk
    It is inherited by base_system.mk as of QPR2
    
    Change-Id: I020a05b233f61ea28d03199cbc76051e6e97e1dc

--- lineage_mata.mk
+++ lineage_mata.mk
@@ -8,9 +8,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk
 # Vendor blobs
 $(call inherit-product, vendor/essential/mata/mata-vendor.mk)
 
-# Enable updating of APEXes
-$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
-
 # Device
 $(call inherit-product, device/essential/mata/device.mk)
 
