347d4d3ede14a77956c79631e739061e79ebe359
Nolen Johnson <johnsonnolen@gmail.com>
2024-03-21 17:07:42 -0400

    mata: Drop legacy trust HAL
    * Dead, will need to move to newer USB HAL at some
      point if we decide to.
    
    Change-Id: Ic356186642f3a7fc777e2ddc956dbb49842ef451

--- device.mk
+++ device.mk
@@ -397,10 +397,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libtinyxml2
 
-# Trust HAL
-PRODUCT_PACKAGES += \
-    vendor.lineage.trust@1.0-service
-
 # Update engine
 PRODUCT_PACKAGES += \
     update_engine \
