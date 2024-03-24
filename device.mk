6ad388db5bf2e181a15a810b4c0d3805f44be9cb
Michael Bestas <mkbestas@lineageos.org>
2024-03-24 13:19:49 +0100

    mata: Switch to android.hardware.usb@1.3-service.dual_role_usb
    Change-Id: Ibd14f7d31dfaf41e040302eda3bc49f351deb618

--- device.mk
+++ device.mk
@@ -408,7 +408,7 @@ PRODUCT_PACKAGES_DEBUG += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service
+    android.hardware.usb@1.3-service.dual_role_usb
 
 # Vibrator
 PRODUCT_PACKAGES += \
