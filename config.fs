5b684c679541e1825ff5b6a29de08003aadf9606
Brandon Solis <bsolis@codeaurora.org>
2024-04-25 10:29:52 +0200

    mata: Update user for ims daemons
    Change user for imsdatadaemon, imsrcsd,
    ims_rtp_daemon
    from AID_SYSTEM to AID_RADIO
    
    Change-Id: I8ee1c4ff182af0d9b4ebf028e3096cfa88118287

--- config.fs
+++ config.fs
@@ -12,19 +12,19 @@ caps: NET_BIND_SERVICE BLOCK_SUSPEND NET_ADMIN
 
 [vendor/bin/imsdatadaemon]
 mode: 0755
-user: AID_SYSTEM
-group: AID_SYSTEM
+user: AID_RADIO
+group: AID_RADIO
 caps: NET_BIND_SERVICE
 
 [vendor/bin/imsrcsd]
 mode: 0755
-user: AID_SYSTEM
+user: AID_RADIO
 group: AID_RADIO
 caps: WAKE_ALARM
 
 [vendor/bin/ims_rtp_daemon]
 mode: 0755
-user: AID_SYSTEM
+user: AID_RADIO
 group: AID_RADIO
 caps: NET_BIND_SERVICE
 
