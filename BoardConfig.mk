db73a48748ef27eea4575830fab1a8daa5dad6fc
Han Sol Jin <hansol@hansol.ca>
2024-05-18 09:42:32 -0700

    mata: explicitly include common Lineage libperfmgr sepolicy
    Change-Id: I37cfc4de02af9f7711bf1fc32aabbbe539017c24

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -130,6 +130,7 @@ VENDOR_SECURITY_PATCH := 2020-02-05
 
 # SELinux
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
+include device/lineage/sepolicy/libperfmgr/sepolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 PRODUCT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 PRODUCT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
