056060b1036f4f80f3ef73afee91dd2934db4119
Tommaso Fonda <fondatommaso2@gmail.com>
2024-11-05 13:18:17 +0100

    mata: Build libhidlbase-v32.recovery
    Change-Id: Ic00339c0b2d84f05d41b9059c089889fd2d230ff

--- device.mk
+++ device.mk
@@ -205,6 +205,7 @@ PRODUCT_COPY_FILES += \
 
 # HIDL
 PRODUCT_PACKAGES += \
+    libhidlbase-v32.recovery \
     libhidlbase-v32.vendor \
     libhidltransport.vendor \
     libhwbinder.vendor
@@ -445,7 +446,6 @@ PRODUCT_PACKAGES += \
 
 # VNDK
 PRODUCT_COPY_FILES += \
-    prebuilts/vndk/v32/arm64/arch-arm64-armv8-a/shared/vndk-sp/libhidlbase.so:$(TARGET_COPY_OUT_RECOVERY)/root/system/lib64/libhidlbase-v32.so \
     prebuilts/vndk/v32/arm64/arch-arm64-armv8-a/shared/llndk-stub/libvndksupport.so:$(TARGET_COPY_OUT_RECOVERY)/root/system/lib64/libvndksupport.so
 
 # Update this list with what each blob is actually for
