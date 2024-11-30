90086863a7977571af7489d4bf19634721a16247
Ricky Cheung <rcheung844@gmail.com>
2024-11-30 16:14:26 +0800

    mata: Transform original power HAL for camera legacy support
    This is a patch to support perf-related features for our camera HAL,
    which does not support the new AIDLs.
    
    The motivation to strip out other items is that none of the other
    services require PowerHAL 1.2 support except the camera HAL.
    
    Since we cannot source compile the camera HAL at the moment, this
    is the cleanest way to accomplish this without introducing a mostly
    unsupported feature into the common HAL.
    
    Change-Id: Ibab7f0f1df4f186ea1eecb4019fb53ca90993ccb

--- device.mk
+++ device.mk
@@ -343,11 +343,13 @@ PRODUCT_COPY_FILES += \
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service.lineage-libperfmgr \
+    android.hardware.power@1.3-service.mata-libperfmgr \
     android.hardware.power.stats@1.0-service.pixel \
     libqti-perfd-client
 
 PRODUCT_COPY_FILES += \
-    device/essential/mata/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+    device/essential/mata/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json \
+    device/essential/mata/configs/powerhint_ext.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint_ext.json
 
 # Protobuf
 PRODUCT_PACKAGES += \
