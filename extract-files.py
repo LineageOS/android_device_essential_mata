bb771dcb011328cb60ad72d699a9cd394ed10162
Nolen Johnson <johnsonnolen@gmail.com>
2024-12-29 13:49:19 -0500

    mata: fix ELF checks for vendor.qti.hardware.qccsyshal@1.0
    Change-Id: Ie265887b86462ab2af6f1d8a475211b14c5e62a4

--- extract-files.py
+++ extract-files.py
@@ -37,6 +37,7 @@ lib_fixups: lib_fixups_user_type = {
     (
         'com.qualcomm.qti.dpm.api@1.0',
         'vendor.qti.imsrtpservice@3.0',
+        'vendor.qti.hardware.qccsyshal@1.0',
     ): lib_fixup_vendor_suffix,
     (
         'libmm-omxcore',
