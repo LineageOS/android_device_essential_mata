c9ae69db325775a88ee95ef23b5a48179dc54233
Nolen Johnson <johnsonnolen@gmail.com>
2024-12-29 13:50:34 -0500

    mata: Fix ELF checks for camera/OMX libraries
    * Drop the useless ones, pull in the ones we are missing.
    
    Change-Id: Ib89b59aaf0635ee2c5ceca06996b30d83bb8f24e

--- extract-files.py
+++ extract-files.py
@@ -36,6 +36,12 @@ lib_fixups: lib_fixups_user_type = {
     **lib_fixups,
     (
         'com.qualcomm.qti.dpm.api@1.0',
+        'libchromaflash',
+        'libdualcameraddm',
+        'liboptizoom',
+        'liboptizoom',
+        'libtrueportrait',
+        'libubifocus',
         'vendor.qti.imsrtpservice@3.0',
         'vendor.qti.hardware.qccsyshal@1.0',
     ): lib_fixup_vendor_suffix,
