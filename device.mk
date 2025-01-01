05554dea6e724d1d5b487247ff8d46c865cd916c
Michael Bestas <mkbestas@lineageos.org>
2025-01-01 04:12:35 +0200

    mata: Update display flags & packages
    Required after display HAL blueprint conversion
    
    [npjohnson]: Unlike most QCOM, mata sets `androidboot.hardware`
    to `mata` instead of `qcom`, so we need to set
    `ro.hardware.hwcomposer` to reflect this.
    
    Change-Id: Idb002fc81fca3321f70fb3bba83f4af44354ce81

--- device.mk
+++ device.mk
@@ -135,7 +135,7 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.1-service \
     android.hardware.graphics.mapper@2.0-impl-2.1 \
     gralloc.msm8998 \
-    hwcomposer.msm8998 \
+    hwcomposer.qcom \
     libdisplayconfig \
     libvulkan \
     vendor.qti.hardware.memtrack-service
