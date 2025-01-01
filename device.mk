60bfd31ef49f397f569ed05529287680d5b2fe86
Michael Bestas <mkbestas@lineageos.org>
2025-01-01 04:12:35 +0200

    mata: Update display flags & packages
    Required after display HAL blueprint conversion
    
    Change-Id: Idb002fc81fca3321f70fb3bba83f4af44354ce81

--- device.mk
+++ device.mk
@@ -139,8 +139,13 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.composer@2.1-service \
     android.hardware.graphics.mapper@2.0-impl-2.1 \
+<<<<<<< PATCH SET (6f2740 mata: Update display flags & packages)
+    gralloc.qcom \
+    hwcomposer.qcom \
+=======
     gralloc.msm8998 \
     hwcomposer.msm8998 \
+>>>>>>> BASE      (91d168 mata: Unset BUILD_BROKEN_INCORRECT_PARTITION_IMAGES)
     libdisplayconfig \
     libqdMetaData.system \
     libtinyxml \
