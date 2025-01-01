6f2740bfbe68b8782425d2387ae8c89e1a3a5473
Michael Bestas <mkbestas@lineageos.org>
2025-01-01 04:12:35 +0200

    mata: Update display flags & packages
    Required after display HAL blueprint conversion
    
    Change-Id: Idb002fc81fca3321f70fb3bba83f4af44354ce81

--- device.mk
+++ device.mk
@@ -139,10 +139,8 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.composer@2.1-service \
     android.hardware.graphics.mapper@2.0-impl-2.1 \
-    android.hardware.memtrack@1.0-impl \
-    android.hardware.memtrack@1.0-service \
-    gralloc.msm8998 \
-    hwcomposer.msm8998 \
+    gralloc.qcom \
+    hwcomposer.qcom \
     libdisplayconfig \
     libqdMetaData.system \
     libtinyxml \
