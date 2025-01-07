2cef21cb4e74c9625e1cfd184421b010ee7425bc
Nolen Johnson <johnsonnolen@gmail.com>
2025-01-07 18:57:11 -0500

    Revert "mata: Disable ELF checks on SDM"
    This reverts commit 877930f7a167443367b429ea1ce0d814bd29bb4c.
    
    Reason: This is in BP now.
    Change-Id: Ifd82e965c3fbc32ab99dabdf8f59392e443cd454

--- extract-files.py
+++ extract-files.py
@@ -47,10 +47,13 @@ lib_fixups: lib_fixups_user_type = {
         'vendor.qti.hardware.qccvndhal@1.0',
     ): lib_fixup_vendor_suffix,
     (
-        'libdrmutils',
         'libmm-omxcore',
         'libOmxCore',
+<<<<<<< PATCH SET (0c054f Revert "mata: Disable ELF checks on SDM")
+        'libril',
+=======
         'libsdmutils',
+>>>>>>> BASE      (8f7a96 mata: Enable ELF checks for libril-qcril-hook-oem.so)
         'libwpa_client',
     ): lib_fixup_remove,
 }
