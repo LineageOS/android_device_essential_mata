9da0738470a2e9b06527805e1a09d977649b0d26
Michael Bestas <mkbestas@lineageos.org>
2025-02-10 19:39:43 +0200

    mata: Enable ELF checks for libOmxVpp
    Change-Id: I2b43f5f7e80b575928b776ea72cb4087025896e9

--- extract-files.py
+++ extract-files.py
@@ -47,8 +47,6 @@ lib_fixups: lib_fixups_user_type = {
         'vendor.qti.hardware.qccvndhal@1.0',
     ): lib_fixup_vendor_suffix,
     (
-        'libmm-omxcore',
-        'libOmxCore',
         'libwpa_client',
     ): lib_fixup_remove,
 }
