e6e4e82e4c4da36664ea892848e146d58f072057
Cosmin Tanislav <demonsingur@gmail.com>
2025-02-19 02:44:05 +0200

    mata: switch to common QCOM AIDL bootctrl HAL
    Change-Id: I04ab771d3b1c38b58913607fbff1bb3b55e1fe25

--- device.mk
+++ device.mk
@@ -88,11 +88,8 @@ TARGET_SCREEN_WIDTH := 1316
 
 # Boot control
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.0-impl:64 \
-    android.hardware.boot@1.0-impl.recovery:64 \
-    android.hardware.boot@1.0-service \
-    bootctrl.msm8998 \
-    bootctrl.msm8998.recovery
+    android.hardware.boot-service.qti \
+    android.hardware.boot-service.qti.recovery
 
 PRODUCT_PACKAGES_DEBUG += \
     bootctl
