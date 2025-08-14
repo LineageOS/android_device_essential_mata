69c0303c9f17c897c541de52d82869d651fceed0
LuK1337 <priv.luk@gmail.com>
2025-08-14 23:47:18 +0200

    mata: Migrate to AIDL LiveDisplay SDM/sysfs HALs
    Change-Id: I580e0721b050480ed5a9ca8bb145f9ef1f1b174f

--- device.mk
+++ device.mk
@@ -229,8 +229,10 @@ PRODUCT_PACKAGES += \
 
 # LiveDisplay native
 PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm \
-    vendor.lineage.livedisplay@2.0-service-sysfs
+    vendor.lineage.livedisplay-service.sdm \
+    vendor.lineage.livedisplay-service.sysfs
+
+$(call soong_config_set,livedisplay_sysfs,enable_ab,true)
 
 # Lineage Health
 PRODUCT_PACKAGES += \
