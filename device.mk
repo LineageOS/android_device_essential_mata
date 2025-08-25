91dcd3c2c605d1d4cc3edf2a438d69b123c8e193
Nolen Johnson <johnsonnolen@gmail.com>
2025-08-24 20:57:36 -0400

    mata: Address hardware/qcom-cag/bt migration to blueprint
    * Boardflags are dead, include the namespace.
    
    Change-Id: Ib9028745531b1a7ae93e18e7298b9198a200b202

--- device.mk
+++ device.mk
@@ -350,6 +350,7 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/google/interfaces \
     hardware/google/pixel \
     hardware/lineage/interfaces/power-libperfmgr \
+    hardware/qcom-caf/bt/libbt-vendor \
     hardware/qcom-caf/common/libqti-perfd-client
 
 # Telephony
